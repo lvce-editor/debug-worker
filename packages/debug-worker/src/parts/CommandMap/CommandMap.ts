3af3795f4e5e533d48b79a86306d563af1f03433
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-27 12:12:20 +0200

    feature: add getKeyBindings (#13)

--- packages/debug-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/debug-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,11 @@
 import * as Create from '../Create/Create.ts'
+import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
+import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
   'RunAndDebug.create': Create.create,
   'RunAndDebug.loadContent': LoadContent.loadContent,
+  'RunAndDebug.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'RunAndDebug.terminate': Terminate.terminate,
 }
