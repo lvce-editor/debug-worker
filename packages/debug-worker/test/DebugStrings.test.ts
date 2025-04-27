fe8bda1919a09e9995ef6055cc2df59d5e8efc9f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-27 19:56:47 +0200

    feature: test strings (#19)
    * feature: test strings
    
    * test

---
+++ packages/debug-worker/test/DebugStrings.test.ts
@@ -0,0 +1,110 @@
+import { test, expect } from '@jest/globals'
+import * as DebugStrings from '../src/parts/DebugStrings/DebugStrings.ts'
+
+test('local', () => {
+  expect(DebugStrings.local()).toBe('Local')
+})
+
+test('namedClosure', () => {
+  expect(DebugStrings.namedClosure('test')).toBe('Closure (test)')
+})
+
+test('closure', () => {
+  expect(DebugStrings.closure()).toBe('Closure')
+})
+
+test('global', () => {
+  expect(DebugStrings.global()).toBe('Global')
+})
+
+test('block', () => {
+  expect(DebugStrings.block()).toBe('Block')
+})
+
+test('expression', () => {
+  expect(DebugStrings.expression()).toBe('Expression')
+})
+
+test('module', () => {
+  expect(DebugStrings.module()).toBe('Module')
+})
+
+test('evalScope', () => {
+  expect(DebugStrings.evalScope()).toBe('Eval')
+})
+
+test('script', () => {
+  expect(DebugStrings.script()).toBe('Script')
+})
+
+test('withScope', () => {
+  expect(DebugStrings.withScope()).toBe('`With` block')
+})
+
+test('catchScope', () => {
+  expect(DebugStrings.catchScope()).toBe('`Catch` block')
+})
+
+test('debuggerPaused', () => {
+  expect(DebugStrings.debuggerPaused()).toBe('Debugger paused')
+})
+
+test('debuggerPausedOnException', () => {
+  expect(DebugStrings.debuggerPausedOnException()).toBe('Paused on exception')
+})
+
+test('stepInto', () => {
+  expect(DebugStrings.stepInto()).toBe('Step Into')
+})
+
+test('stepOver', () => {
+  expect(DebugStrings.stepOver()).toBe('Step Over')
+})
+
+test('stepOut', () => {
+  expect(DebugStrings.stepOut()).toBe('Step Out')
+})
+
+test('pause', () => {
+  expect(DebugStrings.pause()).toBe('Pause')
+})
+
+test('watch', () => {
+  expect(DebugStrings.watch()).toBe('Watch')
+})
+
+test('breakPoints', () => {
+  expect(DebugStrings.breakPoints()).toBe('BreakPoints')
+})
+
+test('scope', () => {
+  expect(DebugStrings.scope()).toBe('Scope')
+})
+
+test('callStack', () => {
+  expect(DebugStrings.callStack()).toBe('Call Stack')
+})
+
+test('notPaused', () => {
+  expect(DebugStrings.notPaused()).toBe('Not Paused')
+})
+
+test('resume', () => {
+  expect(DebugStrings.resume()).toBe('Resume')
+})
+
+test('restart', () => {
+  expect(DebugStrings.restart()).toBe('Restart')
+})
+
+test('stop', () => {
+  expect(DebugStrings.stop()).toBe('Stop')
+})
+
+test('pauseOnExceptions', () => {
+  expect(DebugStrings.pauseOnExceptions()).toBe('Pause on Exceptions')
+})
+
+test('pauseOnUncaughtExceptions', () => {
+  expect(DebugStrings.pauseOnUncaughtExceptions()).toBe('Pause on uncaught Exceptions')
+})
