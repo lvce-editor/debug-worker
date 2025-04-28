e33e1bba1e42b86dee7919242af231d2bcbe32ae
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-28 19:14:23 +0200

    feature: improve debug actions rendering (#28)

--- packages/debug-worker/src/parts/GetRunAndDebugButtonsVirtualDom/GetRunAndDebugButtonsVirtualDom.ts
+++ packages/debug-worker/src/parts/GetRunAndDebugButtonsVirtualDom/GetRunAndDebugButtonsVirtualDom.ts
@@ -4,7 +4,7 @@ import * as GetDebugButtons from '../GetDebugButtons/GetDebugButtons.ts'
 import * as GetDebugButtonVirtualDom from '../GetDebugButtonVirtualDom/GetDebugButtonVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getRunAndDebugButtonsVirtualDom = (debugState: any): readonly VirtualDomNode[] => {
+export const getRunAndDebugButtonsVirtualDom = (debugState: number): readonly VirtualDomNode[] => {
   const debugButtons = GetDebugButtons.getDebugButtons(debugState)
   const dom = [
     {
