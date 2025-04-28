e6a033e132fa42669407e04497c737b638d5417c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-28 19:28:07 +0200

    feature: add handleClickDebugButton function (#33)

---
+++ packages/debug-worker/src/parts/HandleClickDebugButton/HandleClickDebugButton.ts
@@ -0,0 +1,5 @@
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+
+export const handleClickDebugButton = async (state: RunAndDebugState, inputName: string): Promise<RunAndDebugState> => {
+  return state
+}
