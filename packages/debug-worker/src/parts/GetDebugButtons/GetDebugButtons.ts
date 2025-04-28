d6dadb7c07efed2005995d2290fc9bb5d04c539c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-28 19:22:16 +0200

    feature: use maskicon enum (#31)

--- packages/debug-worker/src/parts/GetDebugButtons/GetDebugButtons.ts
+++ packages/debug-worker/src/parts/GetDebugButtons/GetDebugButtons.ts
@@ -1,46 +1,47 @@
 import type { DebugButton } from '../DebugButton/DebugButton.ts'
 import * as DebugState from '../DebugState/DebugState.ts'
 import * as ViewletRunAndDebugStrings from '../DebugStrings/DebugStrings.ts'
+import * as MaskIcon from '../MaskIcon/MaskIcon.ts'
 
 export const getDebugButtons = (debugState: number): readonly DebugButton[] => {
   const debugButtons: DebugButton[] = []
   if (debugState === DebugState.Paused) {
     debugButtons.push({
       title: ViewletRunAndDebugStrings.resume(),
-      icon: 'DebugContinue',
+      icon: MaskIcon.DebugContinue,
       fn: 'resume',
     })
   } else {
     debugButtons.push({
       title: ViewletRunAndDebugStrings.pause(),
-      icon: 'DebugPause',
+      icon: MaskIcon.DebugPause,
       fn: 'pause',
     })
   }
   debugButtons.push(
     {
       title: ViewletRunAndDebugStrings.stepOver(),
-      icon: 'DebugStepOver',
+      icon: MaskIcon.DebugStepOver,
       fn: 'stepOver',
     },
     {
       title: ViewletRunAndDebugStrings.stepInto(),
-      icon: 'DebugStepInto',
+      icon: MaskIcon.DebugStepInto,
       fn: 'stepInto',
     },
     {
       title: ViewletRunAndDebugStrings.stepOut(),
-      icon: 'DebugStepOut',
+      icon: MaskIcon.DebugStepOut,
       fn: 'stepOut',
     },
     {
       title: ViewletRunAndDebugStrings.restart(),
-      icon: 'DebugRestart',
+      icon: MaskIcon.DebugRestart,
       fn: 'restart',
     },
     {
       title: ViewletRunAndDebugStrings.stop(),
-      icon: 'DebugStop',
+      icon: MaskIcon.DebugStop,
       fn: 'stop',
     },
   )
