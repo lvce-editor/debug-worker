1a7136a1d187627fb998c3c6d16b13e61c067ff9
Le Vivilet <levivlet@pm.me>
2025-04-29 22:17:34 +0200

    feature: add debug button event listeners

--- packages/debug-worker/src/parts/HandleClickDebugButton/HandleClickDebugButton.ts
+++ packages/debug-worker/src/parts/HandleClickDebugButton/HandleClickDebugButton.ts
@@ -1,5 +1,22 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { pause, resume, stepInto, stepOut, stepOver } from '../HandlePaused/HandlePaused.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const handleClickDebugButton = async (state: RunAndDebugState, inputName: string): Promise<RunAndDebugState> => {
-  return state
+  switch (inputName) {
+    case InputName.DebugPause:
+      return pause(state)
+    case InputName.DebugResume:
+      return resume(state)
+    case InputName.DebugStepInto:
+      return stepInto(state)
+    case InputName.DebugStepOver:
+      return stepOver(state)
+    case InputName.DebugStepOut:
+      return stepOut(state)
+    case InputName.DebugStop:
+      return state
+    default:
+      return state
+  }
 }
