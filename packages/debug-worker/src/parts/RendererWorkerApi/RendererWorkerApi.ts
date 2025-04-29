8b79e7a08f1394954407fede94cd16b9491e1c1f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-29 22:21:15 +0200

    feature: add activateByEvent (#70)

--- packages/debug-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/debug-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -1,6 +1,5 @@
 export interface RendererWorkerApi {
   readonly 'ClipBoard.writeText': (text: string) => Promise<void>
-  readonly 'ExtensionHost.searchFileWithMemory': (uri: string) => Promise<readonly string[]>
   readonly 'ColorTheme.getColorThemeNames': () => Promise<readonly string[]>
   readonly 'ColorTheme.setColorTheme': (id: string) => Promise<void>
   readonly 'ContextMenu.show': (x: number, y: number, id: any, ...args: readonly any[]) => Promise<void>
@@ -8,6 +7,9 @@ export interface RendererWorkerApi {
   readonly 'ExtensionHost.executeCommand': (id: string) => Promise<void>
   readonly 'ExtensionHost.getCommands': () => Promise<readonly any[]>
   readonly 'ExtensionHost.searchFileWithFetch': (uri: string) => Promise<readonly string[]>
+  readonly 'ExtensionHost.searchFileWithHtml': (uri: string) => Promise<readonly string[]>
+  readonly 'ExtensionHost.searchFileWithMemory': (uri: string) => Promise<readonly string[]>
+  readonly 'ExtensionHostManagement.activateByEvent': (event: string) => Promise<void>
   readonly 'Focus.setFocus': (focusKey: number) => Promise<void>
   readonly 'IconTheme.getFileIcon': (options: any) => Promise<string>
   readonly 'IconTheme.getFolderIcon': (options: any) => Promise<string>
@@ -18,5 +20,4 @@ export interface RendererWorkerApi {
   readonly 'Viewlet.openWidget': (id: string) => Promise<void>
   readonly 'Workspace.getPath': () => Promise<string>
   readonly 'Workspace.setPath': (uri: string) => Promise<void>
-  readonly 'ExtensionHost.searchFileWithHtml': (uri: string) => Promise<readonly string[]>
 }
