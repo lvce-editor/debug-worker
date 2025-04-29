3ab954867dc0d6b930851c98bae80cd634735d76
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-29 19:58:56 +0200

    feature: use more exact type (#65)
    * feature: use more exact type
    
    * test
    
    * test

--- packages/debug-worker/test/GetRunAndDebugRowVirtualDom.test.ts
+++ packages/debug-worker/test/GetRunAndDebugRowVirtualDom.test.ts
@@ -1,10 +1,17 @@
 import { expect, test } from '@jest/globals'
+import type { DebugRow } from '../src/parts/DebugRow/DebugRow.ts'
 import { getRunAndDebugRowVirtualDom } from '../src/parts/GetRunAndDebugRowVirtualDom/GetRunAndDebugRowVirtualDom.ts'
 
 test('getRunAndDebugRowVirtualDom - returns virtual dom for row', () => {
-  const row = {
-    type: 'testType',
-    data: 'testData',
+  const row: DebugRow = {
+    type: 0,
+    expanded: false,
+    indent: 0,
+    key: '',
+    name: '',
+    text: '',
+    value: '0',
+    valueType: '',
   }
 
   const result = getRunAndDebugRowVirtualDom(row)
