38ebcef5367835fed1006f0b7334575178311ad8
Le Vivilet <levivlet@pm.me>
2025-04-29 22:42:00 +0200

    feature: add function to create rpc

--- packages/debug-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
+++ packages/debug-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
@@ -1,15 +1,16 @@
-import type { Rpc } from '@lvce-editor/rpc'
+import { type Rpc, MessagePortRpcParent } from '@lvce-editor/rpc'
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
+import * as ParentRpc from '../Rpc/Rpc.ts'
 
 export const createExtensionHostRpc = async (): Promise<Rpc> => {
   // TODO
-  // 1. create messageport
-  // 2. send messageport to renderer worker
-  // 3. renderer worker sends message port to extension host worker
-  // 4. direct connection is now established
+  const { port1, port2 } = GetPortTuple.getPortTuple()
+  await ParentRpc.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', port2)
+  const rpc = await MessagePortRpcParent.create({
+    commandMap: {},
+    messagePort: port1,
+    isMessagePortOpen: true,
+  })
 
-  // alternatively:
-  // 1. let debug worker be launched by extension host worker
-  // 2. direct communication now exists
-  // 3. however, communication with renderer worker would need to be established
-  return {} as any
+  return rpc
 }
