b21b4aca2ce873982297b409b6dacffa9cb16c4e
Le Vivilet <levivlet@pm.me>
2025-04-29 23:40:38 +0200

    feature: start debugger in loadContent function

--- packages/debug-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/debug-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,8 +1,10 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import * as Debug from '../Debug/Debug.ts'
 import * as DebugState from '../DebugState/DebugState.ts'
 
 export const loadContent = async (state: RunAndDebugState, isTest: boolean): Promise<RunAndDebugState> => {
   const debugId = isTest ? 'test-debug' : 'node-debug' // TODO
+  await Debug.start(debugId)
   return {
     ...state,
     debugId,
