9daeccb2356a6ea57be6ff9d50eaa2c3f3c7041e
Le Vivilet <levivlet@pm.me>
2025-04-29 22:34:27 +0200

    feature: add sendPort function

--- packages/debug-worker/src/parts/Rpc/Rpc.ts
+++ packages/debug-worker/src/parts/Rpc/Rpc.ts
@@ -2,11 +2,14 @@ import type { RendererWorkerApi } from '../RendererWorkerApi/RendererWorkerApi.t
 import * as RpcId from '../RpcId/RpcId.ts'
 import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const invoke = <T extends keyof RendererWorkerApi>(
-  method: T,
-  ...params: Parameters<RendererWorkerApi[T]>
-): ReturnType<RendererWorkerApi[T]> => {
+export const invoke = <T extends keyof RendererWorkerApi>(method: T, ...params: Parameters<RendererWorkerApi[T]>): ReturnType<RendererWorkerApi[T]> => {
   const rpc = RpcRegistry.get(RpcId.RendererWorker)
   // @ts-ignore
   return rpc.invoke(method, ...params)
+}
+
+export const invokeAndTransfer = <T extends keyof RendererWorkerApi>(method: T, ...params: Parameters<RendererWorkerApi[T]>): ReturnType<RendererWorkerApi[T]> => {
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
+  // @ts-ignore
+  return rpc.invokeAndTransfer(method, ...params)
 }
