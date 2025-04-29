e4908a5cae63fa7d718b4a4b60d591e6d7f9e22e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-29 22:30:27 +0200

    feature: add todo (#73)

--- packages/debug-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
+++ packages/debug-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
@@ -6,5 +6,10 @@ export const createExtensionHostRpc = async (): Promise<Rpc> => {
   // 2. send messageport to renderer worker
   // 3. renderer worker sends message port to extension host worker
   // 4. direct connection is now established
+
+  // alternatively:
+  // 1. let debug worker be launched by extension host worker
+  // 2. direct communication now exists
+  // 3. however, communication with renderer worker would need to be established
   return {} as any
 }
