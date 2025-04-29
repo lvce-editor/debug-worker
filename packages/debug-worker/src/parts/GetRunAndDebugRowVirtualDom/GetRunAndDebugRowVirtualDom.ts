96ad27e45000ecd8829b428753da7ca60915eed8
Le Vivilet <levivlet@pm.me>
2025-04-29 19:55:35 +0200

    feature: use more exact type

--- packages/debug-worker/src/parts/GetRunAndDebugRowVirtualDom/GetRunAndDebugRowVirtualDom.ts
+++ packages/debug-worker/src/parts/GetRunAndDebugRowVirtualDom/GetRunAndDebugRowVirtualDom.ts
@@ -1,6 +1,8 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { DebugRow } from '../DebugRow/DebugRow.ts'
 import * as GetRunAndDebugRowRenderer from '../GetRunAndDebugRowRenderer/GetRunAndDebugRowRenderer.ts'
 
-export const getRunAndDebugRowVirtualDom = (row: any): any => {
+export const getRunAndDebugRowVirtualDom = (row: DebugRow): readonly VirtualDomNode[] => {
   const renderer = GetRunAndDebugRowRenderer.getRowRenderer(row.type)
   return renderer(row)
 }
