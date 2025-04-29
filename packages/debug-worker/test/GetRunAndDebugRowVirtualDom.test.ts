4f94a30ee19584cdcc17ee895e06c570641ddbfa
Le Vivilet <levivlet@pm.me>
2025-04-29 19:57:47 +0200

    test

--- packages/debug-worker/test/GetRunAndDebugRowVirtualDom.test.ts
+++ packages/debug-worker/test/GetRunAndDebugRowVirtualDom.test.ts
@@ -1,10 +1,17 @@
 import { expect, test } from '@jest/globals'
 import { getRunAndDebugRowVirtualDom } from '../src/parts/GetRunAndDebugRowVirtualDom/GetRunAndDebugRowVirtualDom.ts'
+import { DebugRow } from '../src/parts/DebugRow/DebugRow.ts'
 
 test('getRunAndDebugRowVirtualDom - returns virtual dom for row', () => {
-  const row = {
-    type: 'testType',
-    data: 'testData',
+  const row: DebugRow = {
+    type: 0,
+    expanded: false,
+    indent: 0,
+    key: '',
+    name: '',
+    text: '',
+    value: '0',
+    valueType: '',
   }
 
   const result = getRunAndDebugRowVirtualDom(row)
