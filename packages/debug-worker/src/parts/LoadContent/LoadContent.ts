c6893bf99511f56d9050ac718b96cb61aeac6f14
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-30 19:56:26 +0200

    fix: view waiting for debugger to start and debugger sending events before view is ready (#95)

--- packages/debug-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/debug-worker/src/parts/LoadContent/LoadContent.ts
@@ -4,7 +4,11 @@ import * as DebugState from '../DebugState/DebugState.ts'
 
 export const loadContent = async (state: RunAndDebugState, isTest: boolean): Promise<RunAndDebugState> => {
   const debugId = isTest ? 'test-debug' : 'node-debug' // TODO
-  await Debug.start(debugId)
+
+  // TODO find a way to start debugger without waiting
+  // TODO race condition
+  // TODO maybe have another function loadSlowContent or loadLaterContent that does this
+  void Debug.start(debugId)
   return {
     ...state,
     debugId,
