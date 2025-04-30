9d3cfb8f49e93503989e84bad6dd8ef3e1c6c888
Le Vivilet <levivlet@pm.me>
2025-04-30 19:36:40 +0200

    feature: move port sending into separate file

--- packages/debug-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
+++ packages/debug-worker/src/parts/CreateExtensionHostRpc/CreateExtensionHostRpc.ts
@@ -1,14 +1,12 @@
 import { type Rpc, MessagePortRpcParent } from '@lvce-editor/rpc'
 import { VError } from '@lvce-editor/verror'
 import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
-import * as ParentRpc from '../Rpc/Rpc.ts'
-import * as RpcId from '../RpcId/RpcId.ts'
+import { sendMessagePortToExtensionHostWorker } from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
 
 export const createExtensionHostRpc = async (): Promise<Rpc> => {
   try {
     const { port1, port2 } = GetPortTuple.getPortTuple()
-    const command = 'HandleMessagePort.handleMessagePort'
-    await ParentRpc.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', port2, command, RpcId.DebugWorker)
+    await sendMessagePortToExtensionHostWorker(port2)
     port1.start()
     const rpc = await MessagePortRpcParent.create({
       commandMap: {},
