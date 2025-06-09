db1435fb13bdcc93e290f71e81114ade7cbba005
Le Vivilet <levivlet@pm.me>
2025-06-09 21:48:25 +0200

    fix: debug worker being started twice

--- packages/debug-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/debug-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,14 +1,8 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import * as Debug from '../Debug/Debug.ts'
 import * as DebugState from '../DebugState/DebugState.ts'
 
 export const loadContent = async (state: RunAndDebugState, isTest: boolean): Promise<RunAndDebugState> => {
   const debugId = isTest ? 'test-debug' : 'node-debug' // TODO
-
-  // TODO find a way to start debugger without waiting
-  // TODO race condition
-  // TODO maybe have another function loadSlowContent or loadLaterContent that does this
-  void Debug.start(debugId)
   return {
     ...state,
     debugId,
