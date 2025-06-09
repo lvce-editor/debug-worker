5b839dc35715800311a9bdfca45c826df34217cc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-09 14:30:57 +0200

    feature: add debug file system provider (#142)

--- packages/debug-worker/src/parts/ReadFile/ReadFile.ts
+++ packages/debug-worker/src/parts/ReadFile/ReadFile.ts
@@ -1,4 +1,25 @@
+import * as ExtensionHostDebug from '../ExtensionHostDebug/ExtensionHostDebug.ts'
+import * as RunAndDebugStates from '../RunAndDebugStates/RunAndDebugStates.ts'
+
+interface ParsedUri {
+  readonly key: number
+  readonly scriptId: string
+}
+
+const parseUri = (uri: string): ParsedUri => {
+  const parts = uri.split('/')
+  const key = Number.parseInt(parts[0])
+  const scriptId = parts[1]
+  return {
+    key,
+    scriptId,
+  }
+}
+
 export const readFile = async (uri: string): Promise<string> => {
-  // TODO parse debugId and script id from uri
-  return ''
+  const { key, scriptId } = parseUri(uri)
+  const { newState } = RunAndDebugStates.get(key)
+  const { debugId } = newState
+  const content = await ExtensionHostDebug.getScriptSource(debugId, scriptId)
+  return content
 }
