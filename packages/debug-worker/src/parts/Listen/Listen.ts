afae77a0a40b303eb186da10e585e77b2e816808
Le Vivilet <levivlet@pm.me>
2025-06-09 10:34:50 +0200

    feature: rename module

--- packages/debug-worker/src/parts/Listen/Listen.ts
+++ packages/debug-worker/src/parts/Listen/Listen.ts
@@ -1,6 +1,6 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as Rpc from '../ParentRpc/ParentRpc.ts'
+import * as Rpc from '../RendererWorker/RendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
   const rpc = await WebWorkerRpcClient.create({
