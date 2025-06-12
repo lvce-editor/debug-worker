436317db4e81142bb7f5d59bbc5e8445791e8865
Le Vivilet <levivlet@pm.me>
2025-06-12 21:15:29 +0200

    log

--- packages/debug-worker/src/parts/HandleClickSectionHeading/HandleClickSectionHeading.ts
+++ packages/debug-worker/src/parts/HandleClickSectionHeading/HandleClickSectionHeading.ts
@@ -2,7 +2,6 @@ import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { getSectionClickHandler } from '../GetSectionClickHandler/GetSectionClickHandler.ts'
 
 export const handleClickSectionHeading = async (state: RunAndDebugState, id: string): Promise<RunAndDebugState> => {
-  console.log('handle click', id)
   const fn = getSectionClickHandler(id)
   return fn(state)
 }
