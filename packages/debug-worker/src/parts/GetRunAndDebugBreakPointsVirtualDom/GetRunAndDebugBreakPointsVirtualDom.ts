bcca357adeec393f09acf17684d9c3838b164404
Le Vivilet <levivlet@pm.me>
2025-06-12 20:54:39 +0200

    debug

--- packages/debug-worker/src/parts/GetRunAndDebugBreakPointsVirtualDom/GetRunAndDebugBreakPointsVirtualDom.ts
+++ packages/debug-worker/src/parts/GetRunAndDebugBreakPointsVirtualDom/GetRunAndDebugBreakPointsVirtualDom.ts
@@ -1,9 +1,11 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DebugSectionId from '../DebugSectionId/DebugSectionId.ts'
 import * as ViewletRunAndDebugStrings from '../DebugStrings/DebugStrings.ts'
 import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtualDom.ts'
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 const textBreakPoints = text(ViewletRunAndDebugStrings.breakPoints())
@@ -16,7 +18,8 @@ export const renderBreakPoints = (state: RunAndDebugState): readonly VirtualDomN
       className: ClassNames.DebugSectionHeader,
       tabIndex: 0,
       childCount: 2,
-      onClick: 'handleClickSectionBreakPoints',
+      onClick: DomEventListenerFunctions.HandleClickSectionBreakPoints,
+      'data-name': DebugSectionId.BreakPoints,
     },
     breakPointsExpanded ? GetChevronVirtualDom.getChevronDownVirtualDom() : GetChevronVirtualDom.getChevronRightVirtualDom(),
     textBreakPoints,
