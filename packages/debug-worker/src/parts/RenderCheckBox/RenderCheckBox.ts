839a82097c3a3ee9dd768d9ae712db88031bf9fd
Le Vivilet <levivlet@pm.me>
2025-06-12 21:41:38 +0200

    lint

--- packages/debug-worker/src/parts/RenderCheckBox/RenderCheckBox.ts
+++ packages/debug-worker/src/parts/RenderCheckBox/RenderCheckBox.ts
@@ -2,11 +2,11 @@ import type { DebugRow } from '../DebugRow/DebugRow.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputType from '../InputType/InputType.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as VirtualDomHelpers from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-import * as InputType from '../InputType/InputType.ts'
 
 export const renderCheckBox = (row: DebugRow): readonly VirtualDomNode[] => {
   const { text, expanded, name } = row
