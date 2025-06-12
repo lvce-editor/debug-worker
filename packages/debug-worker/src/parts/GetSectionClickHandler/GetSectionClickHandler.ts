584be1d3ccd4849f3b54ad73772c3caad3e2f85c
Le Vivilet <levivlet@pm.me>
2025-06-12 21:09:30 +0200

    lint

--- packages/debug-worker/src/parts/GetSectionClickHandler/GetSectionClickHandler.ts
+++ packages/debug-worker/src/parts/GetSectionClickHandler/GetSectionClickHandler.ts
@@ -1,10 +1,10 @@
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as DebugSectionId from '../DebugSectionId/DebugSectionId.ts'
 import { handleClickSectionBreakPoints } from '../HandleClickSectionBreakPoints/HandleClickSectionBreakPoints.ts'
 import { handleClickSectionCallstack } from '../HandleClickSectionCallstack/HandleClickSectionCallstack.ts'
 import { handleClickSectionScope } from '../HandleClickSectionScope/HandleClickSectionScope.ts'
 import { handleClickSectionUnkown } from '../HandleClickSectionUnknown/HandleClickSectionUnknown.ts'
 import { handleClickSectionWatch } from '../HandleClickSectionWatch/HandleClickSectionWatch.ts'
-import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 
 interface ClickHandler {
   (state: RunAndDebugState): RunAndDebugState | Promise<RunAndDebugState>
--- packages/debug-worker/src/parts/HandleClickSectionHeading/HandleClickSectionHeading.ts
+++ packages/debug-worker/src/parts/HandleClickSectionHeading/HandleClickSectionHeading.ts
@@ -1,5 +1,5 @@
-import { getSectionClickHandler } from '../GetSectionClickHandler/GetSectionClickHandler.ts'
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { getSectionClickHandler } from '../GetSectionClickHandler/GetSectionClickHandler.ts'
 
 export const handleClickSectionHeading = async (state: RunAndDebugState, id: string): Promise<RunAndDebugState> => {
   const fn = getSectionClickHandler(id)
