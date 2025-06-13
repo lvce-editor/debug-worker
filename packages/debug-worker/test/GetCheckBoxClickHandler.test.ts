10aba7a7ad1ad7e3278184c42a5f37e51e0f8a5b
Le Vivilet <levivlet@pm.me>
2025-06-13 22:27:59 +0200

    test

--- packages/debug-worker/test/GetCheckBoxClickHandler.test.ts
+++ packages/debug-worker/test/GetCheckBoxClickHandler.test.ts
@@ -1,7 +1,6 @@
 import { expect, test } from '@jest/globals'
 import { getCheckBoxClickHandler } from '../src/parts/GetCheckBoxClickHandler/GetCheckBoxClickHandler.ts'
 import * as InputName from '../src/parts/InputName/InputName.ts'
-import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
 test('getCheckBoxClickHandler - returns pause on exceptions handler', () => {
   const handler = getCheckBoxClickHandler(InputName.PauseOnExceptions)
@@ -15,20 +14,4 @@ test('getCheckBoxClickHandler - returns pause on uncaught exceptions handler', (
 
 test('getCheckBoxClickHandler - throws error for unknown input name', () => {
   expect(() => getCheckBoxClickHandler('unknown')).toThrow('unknown input name')
-})
-
-test('getCheckBoxClickHandler - pause on exceptions handler returns new state', async () => {
-  const handler = getCheckBoxClickHandler(InputName.PauseOnExceptions)
-  const state = createDefaultState()
-  const newState = await handler(state)
-  expect(newState).toBeDefined()
-  expect(newState).not.toBe(state)
-})
-
-test('getCheckBoxClickHandler - pause on uncaught exceptions handler returns new state', async () => {
-  const handler = getCheckBoxClickHandler(InputName.PauseOnUncaughtExceptions)
-  const state = createDefaultState()
-  const newState = await handler(state)
-  expect(newState).toBeDefined()
-  expect(newState).not.toBe(state)
 })
