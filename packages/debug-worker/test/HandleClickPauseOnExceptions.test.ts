86a711a0f1744acca045c378e05e24041d9d627b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-13 23:16:12 +0200

    feature: add pause test (#220)
    * feature: add pause test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * lint

---
+++ packages/debug-worker/test/HandleClickPauseOnExceptions.test.ts
@@ -0,0 +1,96 @@
+import { test, expect } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as ExceptionBreakPoints from '../src/parts/ExceptionBreakPoints/ExceptionBreakPoints.ts'
+import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
+import { handleClickPauseOnExceptions } from '../src/parts/HandleClickPauseOnExceptions/HandleClickPauseOnExceptions.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
+
+test('handleClickPauseOnExceptions toggles from None to All', async () => {
+  const mockRendererWorker = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Debugger.setPauseOnExceptions') {
+        return Promise.resolve()
+      }
+      if (method === 'ExtensionHostManagement.activateByEvent') {
+        return Promise.resolve()
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RendererWorker.set(mockRendererWorker)
+
+  const mockExtensionHost = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      return Promise.resolve()
+    },
+  })
+  ExtensionHost.set(mockExtensionHost)
+
+  const state = createDefaultState()
+  const newState = await handleClickPauseOnExceptions(state)
+  expect(newState.exceptionBreakPoints).toBe(ExceptionBreakPoints.All)
+})
+
+test('handleClickPauseOnExceptions toggles from Uncaught to All', async () => {
+  const mockRendererWorker = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Debugger.setPauseOnExceptions') {
+        return Promise.resolve()
+      }
+      if (method === 'ExtensionHostManagement.activateByEvent') {
+        return Promise.resolve()
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RendererWorker.set(mockRendererWorker)
+
+  const mockExtensionHost = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      return Promise.resolve()
+    },
+  })
+  ExtensionHost.set(mockExtensionHost)
+
+  const state = {
+    ...createDefaultState(),
+    exceptionBreakPoints: ExceptionBreakPoints.Uncaught,
+  }
+  const newState = await handleClickPauseOnExceptions(state)
+  expect(newState.exceptionBreakPoints).toBe(ExceptionBreakPoints.All)
+})
+
+test('handleClickPauseOnExceptions toggles from All to None', async () => {
+  const mockRendererWorker = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Debugger.setPauseOnExceptions') {
+        return Promise.resolve()
+      }
+      if (method === 'ExtensionHostManagement.activateByEvent') {
+        return Promise.resolve()
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RendererWorker.set(mockRendererWorker)
+
+  const mockExtensionHost = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      return Promise.resolve()
+    },
+  })
+  ExtensionHost.set(mockExtensionHost)
+  const state = {
+    ...createDefaultState(),
+    exceptionBreakPoints: ExceptionBreakPoints.All,
+  }
+  const newState = await handleClickPauseOnExceptions(state)
+  expect(newState.exceptionBreakPoints).toBe(ExceptionBreakPoints.None)
+})
