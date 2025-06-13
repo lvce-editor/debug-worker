06b59e2b8fa9975d8abda2e649b030cda54216fd
Le Vivilet <levivlet@pm.me>
2025-06-13 18:59:25 +0200

    test

--- packages/debug-worker/test/OpenUri.test.ts
+++ packages/debug-worker/test/OpenUri.test.ts
@@ -1,32 +1,22 @@
-import { test, expect } from '@jest/globals'
+import { test, expect, jest } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import { openUri } from '../src/parts/OpenUri/OpenUri.ts'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
 test('openUri calls Main.openUri with correct parameters', async () => {
-  let capturedMethod: string | undefined
-  let capturedArgs: unknown[] | undefined
+  const invoke = jest.fn<(method: string, ...args: unknown[]) => Promise<void>>().mockResolvedValue(undefined)
 
-  const mockRpc = await MockRpc.create({
+  const mockRpc = MockRpc.create({
     commandMap: {},
-    invoke: (method: string, ...args: unknown[]) => {
-      capturedMethod = method
-      capturedArgs = args
-      return Promise.resolve()
-    },
+    invoke,
   })
   RendererWorker.set(mockRpc)
 
   await openUri('file:///test.ts', 'typescript', 1, 2)
 
-  expect(capturedMethod).toBe('Main.openUri')
-  expect(capturedArgs).toEqual([
-    'file:///test.ts',
-    true,
-    {
-      languageId: 'typescript',
-      rowIndex: 1,
-      columnIndex: 2,
-    },
-  ])
+  expect(invoke).toHaveBeenCalledWith('Main.openUri', 'file:///test.ts', true, {
+    languageId: 'typescript',
+    rowIndex: 1,
+    columnIndex: 2,
+  })
 })
