a272115320298998ffcf812890b3b847472209ae
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-13 23:20:55 +0200

    fix: dispose rpc (#221)
    * fix: dispose rpc
    
    * lint

--- packages/debug-worker/src/parts/ExtensionHost/ExtensionHost.ts
+++ packages/debug-worker/src/parts/ExtensionHost/ExtensionHost.ts
@@ -1,3 +1,9 @@
+import { get, RpcId } from '@lvce-editor/rpc-registry'
 import { ExtensionHost } from '@lvce-editor/rpc-registry'
 
 export const { invoke, set } = ExtensionHost
+
+export const dispose = async (): Promise<void> => {
+  const rpc = get(RpcId.ExtensionHostWorker)
+  await rpc.dispose()
+}
--- packages/debug-worker/test/InitializeExtensionHost.test.ts
+++ packages/debug-worker/test/InitializeExtensionHost.test.ts
@@ -1,5 +1,6 @@
 import { expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
+import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
 import { initializeExtensionHost } from '../src/parts/InitializeExtensionHost/InitializeExtensionHost.ts'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
@@ -12,6 +13,7 @@ test('initializeExtensionHost - success', async () => {
   })
   RendererWorker.set(mockRpc)
   await initializeExtensionHost()
+  await ExtensionHost.dispose()
   expect(mockInvokeRendererWorker).toHaveBeenCalledTimes(1)
   expect(mockInvokeRendererWorker).toHaveBeenCalledWith(
     'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
