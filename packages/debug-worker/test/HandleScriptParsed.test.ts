9ce305ec13502cc7cc9df1aecb1781ab75c97d32
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-13 22:55:49 +0200

    feature: add parsed script test (#214)
    * feature: add parsed script test
    
    * lint

---
+++ packages/debug-worker/test/HandleScriptParsed.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@jest/globals'
+import type { ParsedScript } from '../src/parts/ParsedScript/ParsedScript.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleScriptParsed } from '../src/parts/HandleScriptParsed/HandleScriptParsed.ts'
+
+test('handleScriptParsed adds parsed script to state', () => {
+  const state = createDefaultState()
+  const parsedScript: ParsedScript = {
+    scriptId: 'test-script',
+    scriptLanguage: 'javascript',
+    url: 'file:///test.js',
+  }
+  const newState = handleScriptParsed(state, parsedScript)
+  expect(newState.parsedScripts['test-script']).toEqual(parsedScript)
+})
+
+test('handleScriptParsed preserves existing parsed scripts', () => {
+  const state = createDefaultState()
+  const existingScript: ParsedScript = {
+    scriptId: 'existing-script',
+    scriptLanguage: 'javascript',
+    url: 'file:///existing.js',
+  }
+  const newScript: ParsedScript = {
+    scriptId: 'new-script',
+    scriptLanguage: 'javascript',
+    url: 'file:///new.js',
+  }
+  const stateWithExisting = handleScriptParsed(state, existingScript)
+  const finalState = handleScriptParsed(stateWithExisting, newScript)
+  expect(finalState.parsedScripts['existing-script']).toEqual(existingScript)
+  expect(finalState.parsedScripts['new-script']).toEqual(newScript)
+})
