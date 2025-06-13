737c62a422a60afd420aa466d19096eb77fcc666
Le Vivilet <levivlet@pm.me>
2025-06-13 19:36:10 +0200

    test

--- packages/debug-worker/test/SendMessagePortToEditorWorker.test.ts
+++ packages/debug-worker/test/SendMessagePortToEditorWorker.test.ts
@@ -1,10 +1,10 @@
-import { expect, test } from '@jest/globals'
+import { test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { sendMessagePortToEditorWorker } from '../src/parts/SendMessagePortToEditorWorker/SendMessagePortToEditorWorker.ts'
 
 test('sendMessagePortToEditorWorker sends port to editor worker', async () => {
-  const mockRpc = await MockRpc.create({
+  const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
       if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker') {
