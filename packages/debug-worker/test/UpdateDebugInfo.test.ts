31189569d154c8f0f1686bdd6e4f29b8a00d53aa
Le Vivilet <levivlet@pm.me>
2025-06-13 18:21:06 +0200

    feature: add debug info test

---
+++ packages/debug-worker/test/UpdateDebugInfo.test.ts
@@ -0,0 +1,19 @@
+import { test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { RpcId } from '@lvce-editor/rpc-registry'
+import { updateDebugInfo } from '../src/parts/UpdateDebugInfo/UpdateDebugInfo.js'
+
+test('updateDebugInfo', async () => {
+  const mockRpc = await MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Editor.updateDebugInfo') {
+        return Promise.resolve()
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RpcId.EditorWorker, mockRpc)
+  await updateDebugInfo('test-key')
+})
