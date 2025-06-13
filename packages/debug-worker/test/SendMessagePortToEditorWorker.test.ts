2672ec5cfe0040145b0003dce9b49894bdfbe088
Le Vivilet <levivlet@pm.me>
2025-06-13 19:39:32 +0200

    test

--- packages/debug-worker/test/SendMessagePortToEditorWorker.test.ts
+++ packages/debug-worker/test/SendMessagePortToEditorWorker.test.ts
@@ -4,17 +4,27 @@ import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { sendMessagePortToEditorWorker } from '../src/parts/SendMessagePortToEditorWorker/SendMessagePortToEditorWorker.ts'
 
 test('sendMessagePortToEditorWorker sends port to editor worker', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
+  const mockRpc = {
     invoke: (method: string) => {
       if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker') {
         return Promise.resolve()
       }
       throw new Error(`unexpected method ${method}`)
     },
-  })
+    invokeAndTransfer: (method: string, port: unknown, command: string, rpcId: unknown) => {
+      if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker') {
+        return Promise.resolve()
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  }
   RendererWorker.set(mockRpc)
 
-  const port = new MessagePort()
-  await sendMessagePortToEditorWorker(port)
+  const mockPort = {
+    postMessage: () => {},
+    start: () => {},
+    close: () => {},
+  } as unknown as MessagePort
+
+  await sendMessagePortToEditorWorker(mockPort)
 })
