786b33a6dbfe2d50a9854a14e785bbc2440fe484
Le Vivilet <levivlet@pm.me>
2025-06-13 18:51:50 +0200

    int

--- packages/debug-worker/test/GetSectionClickHandler.test.ts
+++ packages/debug-worker/test/GetSectionClickHandler.test.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@jest/globals'
-import { getSectionClickHandler } from '../src/parts/GetSectionClickHandler/GetSectionClickHandler.ts'
 import * as DebugSectionId from '../src/parts/DebugSectionId/DebugSectionId.ts'
-import { handleClickSectionWatch } from '../src/parts/HandleClickSectionWatch/HandleClickSectionWatch.ts'
+import { getSectionClickHandler } from '../src/parts/GetSectionClickHandler/GetSectionClickHandler.ts'
 import { handleClickSectionBreakPoints } from '../src/parts/HandleClickSectionBreakPoints/HandleClickSectionBreakPoints.ts'
-import { handleClickSectionScope } from '../src/parts/HandleClickSectionScope/HandleClickSectionScope.ts'
 import { handleClickSectionCallstack } from '../src/parts/HandleClickSectionCallstack/HandleClickSectionCallstack.ts'
+import { handleClickSectionScope } from '../src/parts/HandleClickSectionScope/HandleClickSectionScope.ts'
 import { handleClickSectionUnkown } from '../src/parts/HandleClickSectionUnknown/HandleClickSectionUnknown.ts'
+import { handleClickSectionWatch } from '../src/parts/HandleClickSectionWatch/HandleClickSectionWatch.ts'
 
 test('returns watch handler for watch section', () => {
   const handler = getSectionClickHandler(DebugSectionId.Watch)
