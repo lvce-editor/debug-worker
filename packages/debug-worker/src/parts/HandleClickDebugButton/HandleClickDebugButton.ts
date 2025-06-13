506c1e0c3a952d9424a26b6d66954315cee769d9
Le Vivilet <levivlet@pm.me>
2025-06-13 22:59:10 +0200

    feature: move debug button handler into separate file

--- packages/debug-worker/src/parts/HandleClickDebugButton/HandleClickDebugButton.ts
+++ packages/debug-worker/src/parts/HandleClickDebugButton/HandleClickDebugButton.ts
@@ -1,22 +1,7 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import { pause, resume, stepInto, stepOut, stepOver } from '../HandlePaused/HandlePaused.ts'
-import * as InputName from '../InputName/InputName.ts'
+import { getDebugButtonHandler } from '../GetDebugButtonHandler/GetDebugButtonHandler.ts'
 
 export const handleClickDebugButton = async (state: RunAndDebugState, inputName: string): Promise<RunAndDebugState> => {
-  switch (inputName) {
-    case InputName.DebugPause:
-      return pause(state)
-    case InputName.DebugResume:
-      return resume(state)
-    case InputName.DebugStepInto:
-      return stepInto(state)
-    case InputName.DebugStepOver:
-      return stepOver(state)
-    case InputName.DebugStepOut:
-      return stepOut(state)
-    case InputName.DebugStop:
-      return state
-    default:
-      return state
-  }
+  const fn = getDebugButtonHandler(inputName)
+  return fn(state)
 }
