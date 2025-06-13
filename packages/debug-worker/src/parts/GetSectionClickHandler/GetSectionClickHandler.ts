cef705594aacba924773a9b5911f5addcc65b20e
Le Vivilet <levivlet@pm.me>
2025-06-13 22:47:37 +0200

    feature: move section click handler into separate file

--- packages/debug-worker/src/parts/GetSectionClickHandler/GetSectionClickHandler.ts
+++ packages/debug-worker/src/parts/GetSectionClickHandler/GetSectionClickHandler.ts
@@ -1,16 +1,12 @@
-import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as DebugSectionId from '../DebugSectionId/DebugSectionId.ts'
 import { handleClickSectionBreakPoints } from '../HandleClickSectionBreakPoints/HandleClickSectionBreakPoints.ts'
 import { handleClickSectionCallstack } from '../HandleClickSectionCallstack/HandleClickSectionCallstack.ts'
 import { handleClickSectionScope } from '../HandleClickSectionScope/HandleClickSectionScope.ts'
 import { handleClickSectionUnkown } from '../HandleClickSectionUnknown/HandleClickSectionUnknown.ts'
 import { handleClickSectionWatch } from '../HandleClickSectionWatch/HandleClickSectionWatch.ts'
+import { SectionClickHandler } from '../SectionClickHandler/SectionClickHandler.ts'
 
-interface ClickHandler {
-  (state: RunAndDebugState): RunAndDebugState | Promise<RunAndDebugState>
-}
-
-export const getSectionClickHandler = (id: string): ClickHandler => {
+export const getSectionClickHandler = (id: string): SectionClickHandler => {
   switch (id) {
     case DebugSectionId.Watch:
       return handleClickSectionWatch
