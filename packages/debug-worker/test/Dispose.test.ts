15ea66fc3dcb0f7a5a37d59a4aee7d0dd5958c4c
Le Vivilet <levivlet@pm.me>
2025-06-13 22:28:53 +0200

    dispose

--- packages/debug-worker/test/Dispose.test.ts
+++ packages/debug-worker/test/Dispose.test.ts
@@ -1,10 +1,12 @@
-import { expect, jest, test } from '@jest/globals'
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { dispose } from '../src/parts/Dispose/Dispose.ts'
 import * as RunAndDebugStates from '../src/parts/RunAndDebugStates/RunAndDebugStates.ts'
 
 test('dispose', async () => {
-  const mockDispose = jest.spyOn(RunAndDebugStates, 'dispose')
   const uid = 123
+  const state = createDefaultState()
+  RunAndDebugStates.set(uid, state, state)
   await dispose(uid)
-  expect(mockDispose).toHaveBeenCalledWith(uid)
+  expect(RunAndDebugStates.get(uid)).toBeUndefined()
 })
