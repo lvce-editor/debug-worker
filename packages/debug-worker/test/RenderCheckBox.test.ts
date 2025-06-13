d1d70b08975eecd8fbec43d49f0fc71b15ffed6f
Le Vivilet <levivlet@pm.me>
2025-06-13 18:39:39 +0200

    feature: add checkbox test

---
+++ packages/debug-worker/test/RenderCheckBox.test.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@jest/globals'
+import { renderCheckBox } from '../src/parts/RenderCheckBox/RenderCheckBox.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+import * as InputType from '../src/parts/InputType/InputType.ts'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+test('renderCheckBox', () => {
+  const row = {
+    type: 1,
+    text: 'test',
+    expanded: true,
+    key: 'test-key',
+    value: 'test-value',
+    indent: 0,
+    valueType: 'string',
+    name: 'test-name',
+  }
+  const result = renderCheckBox(row)
+  expect(result).toHaveLength(4)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: expect.any(String),
+    role: AriaRoles.TreeItem,
+    ariaLevel: 2,
+    childCount: 2,
+  })
+  expect(result[1]).toEqual({
+    type: VirtualDomElements.Input,
+    inputType: InputType.CheckBox,
+    name: 'test-name',
+    checked: true,
+    childCount: 0,
+    onChange: DomEventListenerFunctions.HandleClickCheckBox,
+    id: 'test-name',
+  })
+  expect(result[2]).toEqual({
+    type: VirtualDomElements.Label,
+    className: ClassNames.InputLabel,
+    htmlFor: 'test-name',
+    childCount: 1,
+  })
+  expect(result[3]).toEqual({
+    type: 'text',
+    text: 'test',
+  })
+})
