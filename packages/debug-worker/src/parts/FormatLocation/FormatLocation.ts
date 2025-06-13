f18633a9323185de06e3f92ea11a0a89c75168f3
Le Vivilet <levivlet@pm.me>
2025-06-13 23:58:03 +0200

    feature: improve file name formatting

--- packages/debug-worker/src/parts/FormatLocation/FormatLocation.ts
+++ packages/debug-worker/src/parts/FormatLocation/FormatLocation.ts
@@ -1,3 +1,8 @@
 export const formatLocation = (url: string, rowIndex: number, columnIndex: number): string => {
-  return `${url}:${rowIndex}:${columnIndex}`
+  const lastSlashIndex = url.lastIndexOf('/')
+  if (lastSlashIndex === -1) {
+    return `${url}:${rowIndex}:${columnIndex}`
+  }
+  const part = url.slice(lastSlashIndex + 1)
+  return `${part}:${rowIndex}:${columnIndex}`
 }
