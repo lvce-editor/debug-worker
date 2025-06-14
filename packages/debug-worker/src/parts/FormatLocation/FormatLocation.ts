629dcc94ff6d202689385fa52843d176fb703031
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-14 14:22:50 +0200

    feature: only display row instead of row and column (#237)

--- packages/debug-worker/src/parts/FormatLocation/FormatLocation.ts
+++ packages/debug-worker/src/parts/FormatLocation/FormatLocation.ts
@@ -1,8 +1,8 @@
 export const formatLocation = (url: string, rowIndex: number, columnIndex: number): string => {
   const lastSlashIndex = url.lastIndexOf('/')
   if (lastSlashIndex === -1) {
-    return `${url}:${rowIndex}:${columnIndex}`
+    return `${url}:${rowIndex}`
   }
   const part = url.slice(lastSlashIndex + 1)
-  return `${part}:${rowIndex}:${columnIndex}`
+  return `${part}:${rowIndex}`
 }
