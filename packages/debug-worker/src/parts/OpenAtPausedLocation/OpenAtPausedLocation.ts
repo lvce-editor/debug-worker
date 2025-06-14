f0c5950c37afee9546db7dbe383fcbdb4ad9593d
Le Vivilet <levivlet@pm.me>
2025-06-14 23:10:40 +0200

    feature: support opening node uris

--- packages/debug-worker/src/parts/OpenAtPausedLocation/OpenAtPausedLocation.ts
+++ packages/debug-worker/src/parts/OpenAtPausedLocation/OpenAtPausedLocation.ts
@@ -1,4 +1,5 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import { getDebugUri } from '../GetDebugUri/GetDebugUri.ts'
 import * as OpenUri from '../OpenUri/OpenUri.ts'
 import * as RunAndDebugStates from '../RunAndDebugStates/RunAndDebugStates.ts'
 
@@ -15,7 +16,7 @@ export const openAtPausedLocation = async (): Promise<void> => {
   const first = callStack[0]
   const { functionLocation } = first
   const { scriptId, lineNumber, columnNumber } = functionLocation
-  const uri = `debug:///${key}/${scriptId}`
+  const uri = getDebugUri(key, scriptId)
   const rowIndex = lineNumber
   const columnIndex = columnNumber
   const languageId = 'javascript' // TODO
