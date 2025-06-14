26d66fb5924026e43b72458d5b1d7f12a16dc646
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-14 18:16:05 +0200

    feature: move scope functions into separate files (#257)

---
+++ packages/debug-worker/src/parts/ExpandScopeChain/ExpandScopeChain.ts
@@ -0,0 +1,15 @@
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import * as GetChildScopeChain from '../GetChildScopeChain/GetChildScopeChain.ts'
+
+export const expandScopeChain = async (state: RunAndDebugState, expandedIds: any, scopeChain: any, element: any, index: any, debugId: any): Promise<RunAndDebugState> => {
+  const { cache } = state
+  const newScopeChain = await GetChildScopeChain.getChildScopeChain(cache, index, debugId, scopeChain)
+  const objectId = scopeChain[index].objectId
+  const newExpandedIds = [...expandedIds, objectId]
+  return {
+    ...state,
+    scopeChain: newScopeChain,
+    expandedIds: newExpandedIds,
+    scopeFocusedIndex: index,
+  }
+}
