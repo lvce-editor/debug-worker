124fcea0fea9b0edd981943afb3b29f5e2eaa46c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-14 14:33:40 +0200

    feature: format rows in ui with one based offset (#241)

--- packages/debug-worker/src/parts/FormatLocation/FormatLocation.ts
+++ packages/debug-worker/src/parts/FormatLocation/FormatLocation.ts
@@ -1,8 +1,9 @@
 export const formatLocation = (url: string, rowIndex: number, columnIndex: number): string => {
   const lastSlashIndex = url.lastIndexOf('/')
+  const adjustedRowIndex = rowIndex + 1
   if (lastSlashIndex === -1) {
-    return `${url}:${rowIndex}`
+    return `${url}:${adjustedRowIndex}`
   }
   const part = url.slice(lastSlashIndex + 1)
-  return `${part}:${rowIndex}`
+  return `${part}:${adjustedRowIndex}`
 }
