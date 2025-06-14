454269cc414b983168595261e7733170a35555de
Le Vivilet <levivlet@pm.me>
2025-06-14 14:12:04 +0200

    feature: move scopevalue into separate file

--- packages/debug-worker/src/parts/HandlePaused/HandlePaused.ts
+++ packages/debug-worker/src/parts/HandlePaused/HandlePaused.ts
@@ -23,16 +23,6 @@ export const handlePaused = async (state: RunAndDebugState, params: any): Promis
   }
 }
 
-const getElementIndex = (debugId: any, scopeChain: any, text: string): number => {
-  for (let i = 0; i < scopeChain.length; i++) {
-    const element = scopeChain[i]
-    if (element.key === text) {
-      return i
-    }
-  }
-  return -1
-}
-
 const getCollapsedScopeChain = (cache: any, scopeChain: any, element: any, index: number): any => {
   const indent = element.indent
   for (let i = index + 1; i < scopeChain.length; i++) {
@@ -58,43 +48,6 @@ const getCollapsedScopeChain = (cache: any, scopeChain: any, element: any, index
 // TODO when collapsing, store collapsed elements by parent id in cache
 // TODO when expanding, retrieve items from cache by parent id first
 // if they don't exist, query the actual items
-
-const collapse = (state: RunAndDebugState, expandedIds: readonly any[], scopeChain: any, element: any, index: number): RunAndDebugState => {
-  const { cache } = state
-  const newExpandedIds = Arrays.removeElement(expandedIds, element.objectId)
-  const { newScopeChain, newCache } = getCollapsedScopeChain(cache, scopeChain, element, index)
-  return {
-    ...state,
-    expandedIds: newExpandedIds,
-    scopeChain: newScopeChain,
-    scopeFocusedIndex: index,
-    cache: newCache,
-  }
-}
-
-const expand = async (state: RunAndDebugState, expandedIds: any, scopeChain: any, element: any, index: any, debugId: any): Promise<RunAndDebugState> => {
-  const { cache } = state
-  const newScopeChain = await GetChildScopeChain.getChildScopeChain(cache, index, debugId, scopeChain)
-  const objectId = scopeChain[index].objectId
-  const newExpandedIds = [...expandedIds, objectId]
-  return {
-    ...state,
-    scopeChain: newScopeChain,
-    expandedIds: newExpandedIds,
-    scopeFocusedIndex: index,
-  }
-}
-
-export const handleClickScopeValue = async (state: RunAndDebugState, text: string): Promise<RunAndDebugState> => {
-  const { scopeChain, debugId, expandedIds } = state
-  Focus.setFocus(WhenExpression.FocusDebugScope)
-  const index = getElementIndex(debugId, scopeChain, text)
-  const element = scopeChain[index]
-  if (expandedIds.includes(element.objectId)) {
-    return collapse(state, expandedIds, scopeChain, element, index)
-  }
-  return expand(state, expandedIds, scopeChain, element, index, debugId)
-}
 
 export const resume = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { debugId } = state
@@ -193,3 +146,7 @@ export const focusNext = (state: RunAndDebugState): RunAndDebugState => {
 export const resize = (state: RunAndDebugState, dimensions: any): RunAndDebugState => {
   return { ...state, ...dimensions }
 }
+
+
+
+export {handleClickScopeValue} from '../HandleClickScopeValue/HandleClickScopeValue.ts'
