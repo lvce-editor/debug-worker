5f8cd14fd80c4ddfee5a080f10ad1f2739f4b58d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 18:31:51 +0200

    feature: don't send empty focus context change (#328)

--- packages/debug-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/debug-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -2,8 +2,5 @@ import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 
 export const renderFocusContext = (state: RunAndDebugState): readonly any[] => {
   const { focus } = state
-  if (!focus) {
-    return []
-  }
   return ['Viewlet.setFocusContext', focus]
 }
