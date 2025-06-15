f266b50eaaf43c078f950b7172434bf709048291
Le Vivilet <levivlet@pm.me>
2025-06-15 22:53:24 +0200

    tsc

--- packages/debug-worker/test/CollapseScopeChain.test.ts
+++ packages/debug-worker/test/CollapseScopeChain.test.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@jest/globals'
-import type { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
 import { collapseScopeChain } from '../src/parts/CollapseScopeChain/CollapseScopeChain.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
