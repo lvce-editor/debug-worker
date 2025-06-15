bf4733535ca957bfdad8fe3a9d0897ec0e8d874a
Le Vivilet <levivlet@pm.me>
2025-06-15 18:05:04 +0200

    fix: focus diff

--- packages/debug-worker/test/DiffFocus.test.ts
+++ packages/debug-worker/test/DiffFocus.test.ts
@@ -5,23 +5,23 @@ import { isEqual } from '../src/parts/DiffFocus/DiffFocus.ts'
 test('isEqual - no change', () => {
   const state1 = createDefaultState()
   const state2 = createDefaultState()
-  expect(isEqual(state1, state2)).toBe(false)
+  expect(isEqual(state1, state2)).toBe(true)
 })
 
 test('isEqual - focus added', () => {
   const state1 = createDefaultState()
   const state2 = { ...createDefaultState(), focus: 'test' }
-  expect(isEqual(state1, state2)).toBe(true)
+  expect(isEqual(state1, state2)).toBe(false)
 })
 
 test('isEqual - focus removed', () => {
   const state1 = { ...createDefaultState(), focus: 'test' }
   const state2 = createDefaultState()
-  expect(isEqual(state1, state2)).toBe(true)
+  expect(isEqual(state1, state2)).toBe(false)
 })
 
 test('isEqual - focus changed', () => {
   const state1 = { ...createDefaultState(), focus: 'test1' }
   const state2 = { ...createDefaultState(), focus: 'test2' }
-  expect(isEqual(state1, state2)).toBe(true)
+  expect(isEqual(state1, state2)).toBe(false)
 })
