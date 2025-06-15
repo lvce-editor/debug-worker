b39c7d55853d361f20e7c975d3b1e102f6fc6a03
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 18:57:19 +0200

    feature: update watch value change (#335)
    * feature: update watch value change
    
    * lint

---
+++ packages/debug-worker/test/HandleWatchValueChange.test.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@jest/globals'
+import { addWatchExpression } from '../src/parts/AddWatchExpression/AddWatchExpression.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleWatchValueChange } from '../src/parts/HandleWatchValueChange/HandleWatchValueChange.ts'
+
+test('should update last empty watch expression', () => {
+  const state = createDefaultState()
+  const stateWithNewExpression = addWatchExpression(state, '')
+  const newState = handleWatchValueChange(stateWithNewExpression, 'x + y')
+  expect(newState.watchExpressions).toHaveLength(1)
+  expect(newState.watchExpressions[0]).toEqual({
+    expression: 'x + y',
+    value: null,
+  })
+})
+
+test('should not update if last expression is not empty', () => {
+  const state = createDefaultState()
+  const stateWithExpression = addWatchExpression(state, 'a + b')
+  const newState = handleWatchValueChange(stateWithExpression, 'x + y')
+  expect(newState.watchExpressions).toHaveLength(1)
+  expect(newState.watchExpressions[0]).toEqual({
+    expression: 'a + b',
+    value: null,
+  })
+})
+
+test('should not update if no watch expressions exist', () => {
+  const state = createDefaultState()
+  const newState = handleWatchValueChange(state, 'x + y')
+  expect(newState.watchExpressions).toHaveLength(0)
+})
