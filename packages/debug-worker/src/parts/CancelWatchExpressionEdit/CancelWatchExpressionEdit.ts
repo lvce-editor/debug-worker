f1b229d09713a06e502fea9b88a4eed32db673a8
Le Vivilet <levivlet@pm.me>
2025-06-15 19:48:15 +0200

    feature: move watch cancel into separate file

--- packages/debug-worker/src/parts/CancelWatchExpressionEdit/CancelWatchExpressionEdit.ts
+++ packages/debug-worker/src/parts/CancelWatchExpressionEdit/CancelWatchExpressionEdit.ts
@@ -1,13 +1,11 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { getNewWatchExpressionsCancel } from '../GetNewWatchExpressionsCancel/GetNewWatchExpressionsCancel.ts'
 
 export const cancelWatchExpressionEdit = (state: RunAndDebugState): RunAndDebugState => {
   const { watchExpressions } = state
-  const lastExpression = watchExpressions[watchExpressions.length - 1]
-  if (lastExpression && lastExpression.expression === '') {
-    return {
-      ...state,
-      watchExpressions: watchExpressions.toSpliced(-1, 1),
-    }
+  const newWatchExpressions = getNewWatchExpressionsCancel(watchExpressions)
+  return {
+    ...state,
+    watchExpressions: newWatchExpressions,
   }
-  return state
 }
