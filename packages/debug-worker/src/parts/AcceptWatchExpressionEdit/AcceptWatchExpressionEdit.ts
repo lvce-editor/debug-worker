c26c1d7e220438e3a4e47014f4bc5e9f228125d2
Le Vivilet <levivlet@pm.me>
2025-06-15 19:06:30 +0200

    feature: use edit value for acceptwatchedit

--- packages/debug-worker/src/parts/AcceptWatchExpressionEdit/AcceptWatchExpressionEdit.ts
+++ packages/debug-worker/src/parts/AcceptWatchExpressionEdit/AcceptWatchExpressionEdit.ts
@@ -1,12 +1,22 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 
-export const acceptWatchExpressionEdit = (state: RunAndDebugState): RunAndDebugState => {
+export const acceptWatchExpressionEdit = (state: RunAndDebugState, editingValue: string): RunAndDebugState => {
   const { watchExpressions } = state
   const lastExpression = watchExpressions[watchExpressions.length - 1]
   if (lastExpression && lastExpression.expression === '') {
+    if (editingValue === '') {
+      return {
+        ...state,
+        watchExpressions: watchExpressions.toSpliced(-1, 1),
+        focus: 0,
+      }
+    }
     return {
       ...state,
-      watchExpressions: watchExpressions.toSpliced(-1, 1),
+      watchExpressions: watchExpressions.toSpliced(-1, 1, {
+        expression: editingValue,
+        value: null,
+      }),
       focus: 0,
     }
   }
