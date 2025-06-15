e90ef7e677d6ad83ca37bd5120ae201dc18eccb4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 23:12:23 +0200

    feature: add expandscopechain test (#353)
    * feature: add expandscopechain test
    
    * lint
    
    * test
    
    * testlint
    
    * lint

---
+++ packages/debug-worker/test/ExpandScopeChain.test.ts
@@ -0,0 +1,59 @@
+import { expect, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { RpcId } from '@lvce-editor/rpc-registry'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { expandScopeChain } from '../src/parts/ExpandScopeChain/ExpandScopeChain.ts'
+
+test('expandScopeChain', async () => {
+  const state = createDefaultState()
+  const expandedIds = ['id1']
+  const scopeChain = [{ objectId: 'scope1' }, { objectId: 'scope2' }]
+  const element = {}
+  const index = 1
+  const debugId = 'debug1'
+
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'ExtensionHostDebug.getProperties') {
+        return Promise.resolve({
+          result: {
+            result: [
+              {
+                name: 'prop1',
+                value: { value: 'value1' },
+              },
+            ],
+          },
+        })
+      }
+      if (method === 'ExtensionHostManagement.activateByEvent') {
+        return Promise.resolve()
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+  RpcRegistry.set(RpcId.ExtensionHostWorker, mockRpc)
+
+  const result = await expandScopeChain(state, expandedIds, scopeChain, element, index, debugId)
+
+  expect(result).toEqual({
+    ...state,
+    scopeChain: [
+      { objectId: 'scope1' },
+      { objectId: 'scope2' },
+      {
+        type: 3,
+        key: 'prop1',
+        value: '{"value":"value1"}',
+        valueType: '',
+        objectId: '',
+        indent: Number.NaN,
+      },
+    ],
+    expandedIds: ['id1', 'scope2'],
+    scopeFocusedIndex: 1,
+  })
+})
