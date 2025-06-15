8b586468fc8c6d55890a72a9857448e038f13239
Le Vivilet <levivlet@pm.me>
2025-06-15 14:27:47 +0200

    feature: add input field change function

---
+++ packages/debug-worker/test/HandleInputFieldChange.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@jest/globals'
+import { addWatchExpression } from '../src/parts/AddWatchExpression/AddWatchExpression.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as InputName from '../src/parts/InputName/InputName.ts'
+import { handleInputFieldChange } from '../src/parts/HandleInputFieldChange/HandleInputFieldChange.ts'
+
+test('should update new watch expression', () => {
+  const state = createDefaultState()
+  const stateWithNewExpression = addWatchExpression(state, '')
+  const newState = handleInputFieldChange(stateWithNewExpression, InputName.NewWatchExpression, 'x + y')
+  expect(newState.watchExpressions).toHaveLength(1)
+  expect(newState.watchExpressions[0]).toEqual({
+    expression: 'x + y',
+    value: null,
+  })
+})
+
+test('should not update if name is not new watch expression', () => {
+  const state = createDefaultState()
+  const stateWithNewExpression = addWatchExpression(state, '')
+  const newState = handleInputFieldChange(stateWithNewExpression, 'other-input', 'x + y')
+  expect(newState.watchExpressions).toHaveLength(1)
+  expect(newState.watchExpressions[0]).toEqual({
+    expression: '',
+    value: null,
+  })
+})
+
+test('should not update if last expression is not empty', () => {
+  const state = createDefaultState()
+  const stateWithExpression = addWatchExpression(state, 'a + b')
+  const newState = handleInputFieldChange(stateWithExpression, InputName.NewWatchExpression, 'x + y')
+  expect(newState.watchExpressions).toHaveLength(1)
+  expect(newState.watchExpressions[0]).toEqual({
+    expression: 'a + b',
+    value: null,
+  })
+})
