24fa7bfefffd591d54849bb80935601a5603cd0f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 19:41:11 +0200

    feature: move content into separate file (#343)

--- packages/debug-worker/src/parts/GetRunAndDebugVisibleRowsWatch/GetRunAndDebugVisibleRowsWatch.ts
+++ packages/debug-worker/src/parts/GetRunAndDebugVisibleRowsWatch/GetRunAndDebugVisibleRowsWatch.ts
@@ -4,39 +4,8 @@ import * as DebugRowName from '../DebugRowName/DebugRowName.ts'
 import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
 import * as DebugSectionId from '../DebugSectionId/DebugSectionId.ts'
 import * as DebugStrings from '../DebugStrings/DebugStrings.ts'
+import { getRunAndDebugVisibleRowsWatchContent } from '../GetRunAndDebugVisibleRowsWatchContent/GetRunAndDebugVisibleRowsWatchContent.ts'
 import { getWatchActions } from '../GetWatchActions/GetWatchActions.ts'
-import * as InputName from '../InputName/InputName.ts'
-
-const formatValue = (value: any): string => {
-  return value === null || value === undefined ? '' : String(value)
-}
-
-const renderWatchExpression = (expression: string, value: any, isEditing: boolean): DebugRow => {
-  if (isEditing) {
-    return {
-      type: DebugRowType.InputField,
-      text: '',
-      expanded: false,
-      key: 'new-watch-expression',
-      value: '',
-      indent: 0,
-      valueType: '',
-      name: InputName.WatchExpressionInput,
-      description: '',
-    }
-  }
-  return {
-    type: DebugRowType.WatchExpression,
-    text: expression,
-    expanded: false,
-    key: expression,
-    value: formatValue(value),
-    indent: 0,
-    valueType: '',
-    name: '',
-    description: '',
-  }
-}
 
 export const getRunAndDebugVisibleRowsWatch = (state: RunAndDebugState): readonly DebugRow[] => {
   const { watchExpanded, watchExpressions } = state
@@ -56,23 +25,7 @@ export const getRunAndDebugVisibleRowsWatch = (state: RunAndDebugState): readonl
   ]
 
   if (watchExpanded) {
-    if (watchExpressions.length === 0) {
-      rows.push({
-        type: DebugRowType.WatchMessage,
-        text: DebugStrings.noWatchExpression(),
-        expanded: false,
-        key: '',
-        value: '',
-        indent: 0,
-        valueType: '',
-        name: '',
-        description: '',
-      })
-    } else {
-      for (const watchExpression of watchExpressions) {
-        rows.push(renderWatchExpression(watchExpression.expression, watchExpression.value, watchExpression.isEditing || false))
-      }
-    }
+    rows.push(...getRunAndDebugVisibleRowsWatchContent(watchExpressions))
   }
 
   return rows
