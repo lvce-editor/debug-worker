2b7a5c001768ac92941e0c13688bf906050b956e
Le Vivilet <levivlet@pm.me>
2025-06-15 19:32:55 +0200

    feature: update debug row rendering

--- packages/debug-worker/test/RenderWatchExpression.test.ts
+++ packages/debug-worker/test/RenderWatchExpression.test.ts
@@ -1,12 +1,24 @@
 import { test, expect } from '@jest/globals'
 import { text } from '@lvce-editor/virtual-dom-worker'
+import type { DebugRow } from '../src/parts/DebugRow/DebugRow.ts'
 import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
 import { renderWatchExpression } from '../src/parts/RenderWatchExpression/RenderWatchExpression.ts'
 import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
 
 test('should render watch expression', () => {
-  const result = renderWatchExpression('x + y', '42', 'number')
+  const row: DebugRow = {
+    key: 'x + y',
+    value: '42',
+    text: '',
+    type: 0,
+    description: '',
+    expanded: false,
+    indent: 0,
+    name: '',
+    valueType: '',
+  }
+  const result = renderWatchExpression(row)
   expect(result).toEqual([
     {
       type: VirtualDomElements.Div,
