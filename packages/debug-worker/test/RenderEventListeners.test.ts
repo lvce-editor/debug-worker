1c78958397407f8be1d8a9d5cb1bea3a25ad4f51
Le Vivilet <levivlet@pm.me>
2025-06-15 14:27:00 +0200

    feature: render input change event listener

--- packages/debug-worker/test/RenderEventListeners.test.ts
+++ packages/debug-worker/test/RenderEventListeners.test.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@jest/globals'
 import { renderEventListeners } from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 test('renderEventListeners', () => {
   const eventListeners = renderEventListeners()
-  expect(eventListeners[0]).toMatchObject({
-    name: expect.any(String),
+  expect(eventListeners).toContainEqual({
+    name: DomEventListenerFunctions.HandleClickContinue,
     params: ['handleClickContinue'],
+  })
+  expect(eventListeners).toContainEqual({
+    name: DomEventListenerFunctions.HandleInputFieldChange,
+    params: ['handleInputFieldChange', 'event.target.name', 'event.target.value'],
   })
 })
