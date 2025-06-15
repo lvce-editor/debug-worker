c6d97320e2881213e9b54fbe34b96a561119c4b3
Le Vivilet <levivlet@pm.me>
2025-06-15 14:41:09 +0200

    feature: simplify watchexpressions

--- packages/debug-worker/src/parts/AddWatchExpression/AddWatchExpression.ts
+++ packages/debug-worker/src/parts/AddWatchExpression/AddWatchExpression.ts
@@ -1,14 +1,20 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import type { WatchExpression } from '../WatchExpression/WatchExpression.ts'
 
-export const addWatchExpression = (state: RunAndDebugState, expression: string): RunAndDebugState => {
-  const { watchExpressions } = state
+const getNewWatchExpressions = (watchExpressions: readonly WatchExpression[], expression: string): readonly WatchExpression[] => {
   const watchExpression: WatchExpression = {
     expression,
     value: null,
   }
+  const newWatchExpressions = [...watchExpressions, watchExpression]
+  return newWatchExpressions
+}
+
+export const addWatchExpression = (state: RunAndDebugState, expression: string): RunAndDebugState => {
+  const { watchExpressions } = state
+  const newWatchExpressions = getNewWatchExpressions(watchExpressions, expression)
   return {
     ...state,
-    watchExpressions: [...watchExpressions, watchExpression],
+    watchExpressions: newWatchExpressions,
   }
 }
