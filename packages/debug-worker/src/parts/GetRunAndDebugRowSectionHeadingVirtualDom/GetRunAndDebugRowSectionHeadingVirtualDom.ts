f1b680bf6b7bd15a92ea8816fab5e756c86f22fe
Le Vivilet <levivlet@pm.me>
2025-06-15 17:40:37 +0200

    feature: move debug actions into heading element

--- packages/debug-worker/src/parts/GetRunAndDebugRowSectionHeadingVirtualDom/GetRunAndDebugRowSectionHeadingVirtualDom.ts
+++ packages/debug-worker/src/parts/GetRunAndDebugRowSectionHeadingVirtualDom/GetRunAndDebugRowSectionHeadingVirtualDom.ts
@@ -1,4 +1,4 @@
-import type { DebugRow } from '../DebugRow/DebugRow.ts'
+import type { DebugRow, DebugRowAction } from '../DebugRow/DebugRow.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
@@ -7,8 +7,30 @@ import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtual
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import * as VirtualDomHelpers from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
+const getActionsDom = (actions: readonly DebugRowAction[]): readonly VirtualDomNode[] => {
+  const nodes: VirtualDomNode[] = []
+  nodes.push({
+    type: VirtualDomElements.Div,
+    className: ClassNames.DebugSectionActions,
+    childCount: actions.length,
+  })
+  for (const action of actions) {
+    nodes.push({
+      type: VirtualDomElements.Button,
+      className: ClassNames.DebugSectionAction,
+      title: action.title,
+      onClick: DomEventListenerFunctions.HandleClickSectionAction,
+      name: action.id,
+      childCount: 1,
+    })
+    nodes.push(VirtualDomHelpers.text(action.icon))
+  }
+  return nodes
+}
+
 export const renderSectionHeading = (row: DebugRow): readonly VirtualDomNode[] => {
   const { expanded, text, key, actions } = row
+  const hasActions = actions && actions.length > 0
   const nodes: VirtualDomNode[] = [
     {
       type: VirtualDomElements.Div,
@@ -16,32 +38,13 @@ export const renderSectionHeading = (row: DebugRow): readonly VirtualDomNode[] =
       role: AriaRoles.TreeItem,
       ariaExpanded: expanded,
       ariaLevel: 1,
-      childCount: 2,
+      childCount: hasActions ? 3 : 2,
       onClick: DomEventListenerFunctions.HandleClickSectionHeading,
       'data-name': key,
     },
     expanded ? GetChevronVirtualDom.getChevronDownVirtualDom() : GetChevronVirtualDom.getChevronRightVirtualDom(),
     VirtualDomHelpers.text(text),
+    ...getActionsDom(actions || []),
   ]
-
-  if (actions) {
-    nodes.push({
-      type: VirtualDomElements.Div,
-      className: ClassNames.DebugSectionActions,
-      childCount: actions.length,
-    })
-    for (const action of actions) {
-      nodes.push({
-        type: VirtualDomElements.Button,
-        className: ClassNames.DebugSectionAction,
-        title: action.title,
-        onClick: DomEventListenerFunctions.HandleClickSectionAction,
-        name: action.id,
-        childCount: 1,
-      })
-      nodes.push(VirtualDomHelpers.text(action.icon))
-    }
-  }
-
   return nodes
 }
