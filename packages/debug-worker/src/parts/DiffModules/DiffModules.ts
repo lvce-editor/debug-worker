0d3340e10ea1722e4ccc7bb67b4b3edc8a7d1170
Le Vivilet <levivlet@pm.me>
2025-06-15 14:54:08 +0200

    lint

--- packages/debug-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/debug-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,5 +1,5 @@
-import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 
 export const modules = [DiffItems.isEqual, DiffFocus.isEqual]
