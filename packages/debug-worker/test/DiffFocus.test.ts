02833758fdec67dbce23c1ee4cc8b91480d495bd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 14:55:10 +0200

    feature: add diffFocus to diff modules (#308)
    * feature: add diffFocus to diff modules
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * lint

--- packages/debug-worker/test/DiffFocus.test.ts
+++ packages/debug-worker/test/DiffFocus.test.ts
@@ -1,27 +1,27 @@
 import { test, expect } from '@jest/globals'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
-import { diffFocus } from '../src/parts/DiffFocus/DiffFocus.ts'
+import { isEqual } from '../src/parts/DiffFocus/DiffFocus.ts'
 
-test('diffFocus - no change', () => {
+test('isEqual - no change', () => {
   const state1 = createDefaultState()
   const state2 = createDefaultState()
-  expect(diffFocus(state1, state2)).toBe(false)
+  expect(isEqual(state1, state2)).toBe(false)
 })
 
-test('diffFocus - focus added', () => {
+test('isEqual - focus added', () => {
   const state1 = createDefaultState()
   const state2 = { ...createDefaultState(), focus: 'test' }
-  expect(diffFocus(state1, state2)).toBe(true)
+  expect(isEqual(state1, state2)).toBe(true)
 })
 
-test('diffFocus - focus removed', () => {
+test('isEqual - focus removed', () => {
   const state1 = { ...createDefaultState(), focus: 'test' }
   const state2 = createDefaultState()
-  expect(diffFocus(state1, state2)).toBe(true)
+  expect(isEqual(state1, state2)).toBe(true)
 })
 
-test('diffFocus - focus changed', () => {
+test('isEqual - focus changed', () => {
   const state1 = { ...createDefaultState(), focus: 'test1' }
   const state2 = { ...createDefaultState(), focus: 'test2' }
-  expect(diffFocus(state1, state2)).toBe(true)
+  expect(isEqual(state1, state2)).toBe(true)
 })
