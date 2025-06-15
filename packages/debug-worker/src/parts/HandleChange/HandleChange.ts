ec50feb54b06bbd6bbf42e7224b7966ab88fe280
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 11:41:19 +0200

    feature: add getpausedInfo2 function (#275)

--- packages/debug-worker/src/parts/HandleChange/HandleChange.ts
+++ packages/debug-worker/src/parts/HandleChange/HandleChange.ts
@@ -1,16 +1,35 @@
 import type { ChangeParams } from '../ChangeParams/ChangeParams.ts'
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import * as DebugState from '../DebugState/DebugState.ts'
+import { getPausedInfo2 } from '../GetPausedInfo2/GetPausedInfo2.ts'
 import * as HandleResumed from '../HandleResumed/HandleResumed.ts'
+
+const handlePaused = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
+  const { debugId } = state
+  try {
+    const { callFrameId, callStack, scopeChain, pausedMessage, pausedReason, expandedIds } = await getPausedInfo2(debugId)
+    return {
+      ...state,
+      debugState: DebugState.Paused,
+      scopeChain,
+      scopeExpanded: true,
+      callStack,
+      pausedReason,
+      pausedMessage,
+      callFrameId,
+      expandedIds,
+    }
+  } catch {
+    return state
+  }
+}
 
 export const handleChange = async (state: RunAndDebugState, params: ChangeParams): Promise<RunAndDebugState> => {
   if (params.type === 'resumed') {
     return HandleResumed.handleResumed(state)
   }
   if (params.type === 'paused') {
-    // TODO query status, then callstack, scopechain
-    return {
-      ...state,
-    }
+    return handlePaused(state)
   }
   return {
     ...state,
