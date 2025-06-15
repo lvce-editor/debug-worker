a4f2defc8201fc7fb0e7fe95fa7411f58f098ddb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 19:47:35 +0200

    fix: watch expression accept (#345)

--- packages/debug-worker/src/parts/AcceptWatchExpressionEdit/AcceptWatchExpressionEdit.ts
+++ packages/debug-worker/src/parts/AcceptWatchExpressionEdit/AcceptWatchExpressionEdit.ts
@@ -1,28 +1,12 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { getNewWatchExpressionsAccept } from '../GetNewWatchExpressionsAccept/GetNewWatchExpressionsAccept.ts'
 
-export const acceptWatchExpressionEdit = (state: RunAndDebugState, editingValue: string): RunAndDebugState => {
-  const { watchExpressions } = state
-  const editingIndex = watchExpressions.findIndex((expr) => expr.isEditing)
-
-  if (editingIndex === -1) {
-    return state
-  }
-
-  if (editingValue === '') {
-    return {
-      ...state,
-      watchExpressions: watchExpressions.toSpliced(editingIndex, 1),
-      focus: 0,
-    }
-  }
-
+export const acceptWatchExpressionEdit = (state: RunAndDebugState): RunAndDebugState => {
+  const { watchExpressions, editingValue } = state
+  const newWatchExpressions = getNewWatchExpressionsAccept(watchExpressions, editingValue)
   return {
     ...state,
-    watchExpressions: watchExpressions.toSpliced(editingIndex, 1, {
-      expression: editingValue,
-      value: null,
-      isEditing: false,
-    }),
+    watchExpressions: newWatchExpressions,
     focus: 0,
   }
 }
