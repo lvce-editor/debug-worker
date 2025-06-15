a17f24670f06df8ded9f43585e3b3f6befb8fc36
Le Vivilet <levivlet@pm.me>
2025-06-15 14:02:41 +0200

    feature: add watch expression render

---
+++ packages/debug-worker/src/parts/RenderWatchExpression/RenderWatchExpression.ts
@@ -0,0 +1,28 @@
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as VirtualDomHelpers from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+
+export const renderWatchExpression = (expression: string, value: string, valueType: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.DebugRow,
+      role: AriaRoles.TreeItem,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Span,
+      className: ClassNames.DebugValueScopeName,
+      childCount: 1,
+    },
+    VirtualDomHelpers.text(expression),
+    {
+      type: VirtualDomElements.Span,
+      className: ClassNames.DebugValue,
+      childCount: 1,
+    },
+    VirtualDomHelpers.text(value),
+  ]
+}
