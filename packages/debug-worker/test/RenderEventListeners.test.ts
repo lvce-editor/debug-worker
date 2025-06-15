04aa69968f9bca5266fd621315b4b2a1076dfd11
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-15 14:28:30 +0200

    Discard changes to packages/debug-worker/test/RenderEventListeners.test.ts

--- packages/debug-worker/test/RenderEventListeners.test.ts
+++ packages/debug-worker/test/RenderEventListeners.test.ts
@@ -1,15 +1,10 @@
 import { test, expect } from '@jest/globals'
 import { renderEventListeners } from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
-import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 test('renderEventListeners', () => {
   const eventListeners = renderEventListeners()
-  expect(eventListeners).toContainEqual({
-    name: DomEventListenerFunctions.HandleClickContinue,
+  expect(eventListeners[0]).toMatchObject({
+    name: expect.any(String),
     params: ['handleClickContinue'],
-  })
-  expect(eventListeners).toContainEqual({
-    name: DomEventListenerFunctions.HandleInputFieldChange,
-    params: ['handleInputFieldChange', 'event.target.name', 'event.target.value'],
   })
 })
