54f71b09cf713118322b6d853fa0cc35e9c36358
Le Vivilet <levivlet@pm.me>
2025-06-16 23:41:05 +0200

    feature: register value diff module

--- packages/debug-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/debug-worker/src/parts/DiffModules/DiffModules.ts
@@ -2,7 +2,8 @@ import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffItems.isEqual, DiffFocusContext.isEqual, DiffFocus.isEqual]
+export const modules = [DiffItems.isEqual, DiffFocusContext.isEqual, DiffValue.isEqual, DiffFocus.isEqual]
 
-export const numbers = [DiffItems.diffType, DiffType.RenderFocusContext, DiffType.RenderFocus]
+export const numbers = [DiffItems.diffType, DiffType.RenderFocusContext, DiffType.RenderValue, DiffType.RenderFocus]
