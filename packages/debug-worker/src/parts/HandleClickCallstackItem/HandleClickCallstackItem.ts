3621a07eb35abd6c5b12a2d00da6b23d661c4eae
Le Vivilet <levivlet@pm.me>
2025-06-16 23:22:48 +0200

    feature: add separate parseIndex function

--- packages/debug-worker/src/parts/HandleClickCallstackItem/HandleClickCallstackItem.ts
+++ packages/debug-worker/src/parts/HandleClickCallstackItem/HandleClickCallstackItem.ts
@@ -1,9 +1,10 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { getOpenInfo } from '../GetOpenInfo/GetOpenInfo.ts'
 import * as OpenUri from '../OpenUri/OpenUri.ts'
+import { parseIndex } from '../ParseIndex/ParseIndex.ts'
 
 export const handleClickCallstackItem = async (state: RunAndDebugState, dataIndex: string): Promise<RunAndDebugState> => {
-  const index = Number.parseInt(dataIndex)
+  const index = parseIndex(dataIndex)
   const { callStack, parsedScripts, id } = state
   const item = callStack[index]
   if (!item) {
---
+++ packages/debug-worker/src/parts/ParseIndex/ParseIndex.ts
@@ -0,0 +1,4 @@
+export const parseIndex = (dataIndex: string): number => {
+  const index = Number.parseInt(dataIndex)
+  return index
+}
