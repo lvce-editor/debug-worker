64a949d16cca3b879f29853315fab2249778d471
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-16 23:10:52 +0200

    feature: evaluate watch expression on add (#377)

--- packages/debug-worker/src/parts/AddWatchExpression/AddWatchExpression.ts
+++ packages/debug-worker/src/parts/AddWatchExpression/AddWatchExpression.ts
@@ -1,18 +1,34 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import type { WatchExpression } from '../WatchExpression/WatchExpression.ts'
+import * as DebugState from '../DebugState/DebugState.ts'
+import { evaluateWatchExpressions } from '../EvaluateWatchExpressions/EvaluateWatchExpressions.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
+const getNewWatchExpressions = async (
+  debugId: any,
+  callFrameId: any,
+  debugState: number,
+  watchExpressions: readonly WatchExpression[],
+): Promise<readonly WatchExpression[]> => {
+  if (debugState === DebugState.Paused) {
+    const newWatchExpressions = await evaluateWatchExpressions(debugId, callFrameId, watchExpressions)
+    return newWatchExpressions
+  }
+  return watchExpressions
+}
+
 export const addWatchExpression = async (state: RunAndDebugState, expression: string): Promise<RunAndDebugState> => {
-  const { watchExpressions } = state
+  const { watchExpressions, debugId, callFrameId, debugState } = state
   const watchExpression: WatchExpression = {
     expression,
     value: null,
     isEditing: true,
   }
-  // TODO evaluate watch expresssion if debugger is paused
+  const newWatchExpressions1 = [...watchExpressions, watchExpression]
+  const newWatchExpressions2 = await getNewWatchExpressions(debugId, callFrameId, debugState, newWatchExpressions1)
   return {
     ...state,
-    watchExpressions: [...watchExpressions, watchExpression],
+    watchExpressions: newWatchExpressions2,
     focus: WhenExpression.FocusDebugWatchInput,
   }
 }
