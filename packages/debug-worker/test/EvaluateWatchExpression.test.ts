e72d24fd548fe5c371e2a124bd410575928f007e
Le Vivilet <levivlet@pm.me>
2025-06-16 22:57:02 +0200

    feature: update watch expression evaluation

--- packages/debug-worker/test/EvaluateWatchExpression.test.ts
+++ packages/debug-worker/test/EvaluateWatchExpression.test.ts
@@ -23,26 +23,7 @@ test('evaluateWatchExpression', async () => {
   })
   RendererWorker.set(mockRpc)
   ExtensionHost.set(mockRpc)
-
-  const state: RunAndDebugState = createDefaultState()
   const expression = 'x + y'
-  const stateWithExpression: RunAndDebugState = {
-    ...state,
-    watchExpressions: [
-      {
-        expression,
-        value: null,
-        isEditing: false,
-      },
-    ],
-    debugId: 123,
-  }
-  const result = await evaluateWatchExpression(stateWithExpression, expression)
-  expect(result.watchExpressions).toEqual([
-    {
-      expression,
-      value: 'evaluated result',
-      isEditing: false,
-    },
-  ])
+  const result = await evaluateWatchExpression(123, 456, expression)
+  expect(result).toEqual({ result: 'evaluated result' })
 })
