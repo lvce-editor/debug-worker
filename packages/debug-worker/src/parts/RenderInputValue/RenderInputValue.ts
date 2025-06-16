eb880a3399cdf22dbdddfe9093c59f8165ef0bff
Le Vivilet <levivlet@pm.me>
2025-06-16 23:40:47 +0200

    feature: improve input value render

--- packages/debug-worker/src/parts/RenderInputValue/RenderInputValue.ts
+++ packages/debug-worker/src/parts/RenderInputValue/RenderInputValue.ts
@@ -2,10 +2,10 @@ import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { getFocusSelector } from '../GetFocusSelector/GetFocusSelector.ts'
 
 export const renderInputValue = (state: RunAndDebugState): readonly any[] => {
-  const { id, focus } = state
+  const { id, focus, editingValue } = state
   const selector = getFocusSelector(focus)
   if (!selector) {
-    return ['Viewlet.setValueByName', id, '']
+    return ['Viewlet.setValueByName', id, '', editingValue]
   }
-  return ['Viewlet.setValueByName', id, selector]
+  return ['Viewlet.setValueByName', id, selector, editingValue]
 }
