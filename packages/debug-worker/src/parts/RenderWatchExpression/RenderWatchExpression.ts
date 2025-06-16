e0f36457268644ff9c0de8a74ab7cc3d4ead49a9
Le Vivilet <levivlet@pm.me>
2025-06-16 23:19:24 +0200

    feature: render watch expression index

--- packages/debug-worker/src/parts/RenderWatchExpression/RenderWatchExpression.ts
+++ packages/debug-worker/src/parts/RenderWatchExpression/RenderWatchExpression.ts
@@ -7,13 +7,14 @@ import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts
 import * as VirtualDomHelpers from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const renderWatchExpression = (row: DebugRow): readonly VirtualDomNode[] => {
-  const { value, key } = row
+  const { value, key, index } = row
   return [
     {
       type: VirtualDomElements.Div,
       className: ClassNames.DebugRow,
       role: AriaRoles.TreeItem,
       childCount: 3,
+      'data-index': index,
     },
     {
       type: VirtualDomElements.Span,
