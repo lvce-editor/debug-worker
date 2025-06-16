0a1b1969c738911dae52ba8a3add99cb604e1cdc
Le Vivilet <levivlet@pm.me>
2025-06-16 21:24:04 +0200

    feature: save watch expressions

--- packages/debug-worker/test/SaveState.test.ts
+++ packages/debug-worker/test/SaveState.test.ts
@@ -1,9 +1,17 @@
 import { test, expect } from '@jest/globals'
+import type { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as RunAndDebugStates from '../src/parts/RunAndDebugStates/RunAndDebugStates.ts'
 import { saveState } from '../src/parts/SaveState/SaveState.ts'
 
 test('saveState returns correct state', () => {
-  const result = saveState(1)
+  const id = 1
+  const state: RunAndDebugState = {
+    ...createDefaultState(),
+  }
+  RunAndDebugStates.set(id, state, state)
+  const result = saveState(id)
   expect(result).toEqual({
-    x: 1,
+    watchExpressions: [],
   })
 })
