ac1bfaa691564571858f021122f709c99026a0b2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-16 21:25:33 +0200

    feature: save watch expressions (#369)

--- packages/debug-worker/src/parts/SaveState/SaveState.ts
+++ packages/debug-worker/src/parts/SaveState/SaveState.ts
@@ -1,7 +1,15 @@
 import type { SavedState } from '../SavedState/SavedState.ts'
+import type { WatchExpression } from '../WatchExpression/WatchExpression.ts'
+import * as RunAndDebugStates from '../RunAndDebugStates/RunAndDebugStates.ts'
+
+const serializeWatchExpression = (expression: WatchExpression): string => {
+  return `${expression.value}` || ''
+}
 
 export const saveState = (uid: number): SavedState => {
+  const { newState } = RunAndDebugStates.get(uid)
+  const { watchExpressions } = newState
   return {
-    x: 1,
+    watchExpressions: watchExpressions.map(serializeWatchExpression),
   }
 }
