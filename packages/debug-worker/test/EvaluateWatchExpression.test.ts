7e8d38f2c73c8c269bd09421d1cd9e38039e3d77
Le Vivilet <levivlet@pm.me>
2025-06-16 21:13:49 +0200

    test

--- packages/debug-worker/test/EvaluateWatchExpression.test.ts
+++ packages/debug-worker/test/EvaluateWatchExpression.test.ts
@@ -2,8 +2,28 @@ import { test, expect } from '@jest/globals'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { evaluateWatchExpression } from '../src/parts/EvaluateWatchExpression/EvaluateWatchExpression.ts'
 import { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { RpcId } from '@lvce-editor/rpc-registry'
 
 test('evaluateWatchExpression', async () => {
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Debug.evaluate') {
+        return {
+          result: 'evaluated result',
+        }
+      }
+      if (method === 'ExtensionHostManagement.activateByEvent') {
+        return undefined
+      }
+      return undefined
+    },
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+  RpcRegistry.set(RpcId.ExtensionHostWorker, mockRpc)
+
   const state: RunAndDebugState = createDefaultState()
   const expression = 'x + y'
   const stateWithExpression: RunAndDebugState = {
