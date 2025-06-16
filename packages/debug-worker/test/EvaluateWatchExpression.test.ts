b3c8a69a1fe860d8fe89a8dc3aeba94ab2d522f0
Le Vivilet <levivlet@pm.me>
2025-06-16 23:03:45 +0200

    t

--- packages/debug-worker/test/EvaluateWatchExpression.test.ts
+++ packages/debug-worker/test/EvaluateWatchExpression.test.ts
@@ -1,7 +1,5 @@
 import { expect, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
-import type { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
-import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { evaluateWatchExpression } from '../src/parts/EvaluateWatchExpression/EvaluateWatchExpression.ts'
 import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
