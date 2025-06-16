4f239da615c4646a7812c654a99391dbd081e512
Le Vivilet <levivlet@pm.me>
2025-06-16 23:03:17 +0200

    lint

--- packages/debug-worker/test/HandleWatchValueChange.test.ts
+++ packages/debug-worker/test/HandleWatchValueChange.test.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@jest/globals'
+import type { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
 import { addWatchExpression } from '../src/parts/AddWatchExpression/AddWatchExpression.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { handleWatchValueChange } from '../src/parts/HandleWatchValueChange/HandleWatchValueChange.ts'
 
 test('should update editingValue', () => {
-  const state = createDefaultState()
-  const stateWithEditingValue = handleWatchValueChange(state, 'x + y')
+  const state: RunAndDebugState = createDefaultState()
+  const stateWithEditingValue: RunAndDebugState = handleWatchValueChange(state, 'x + y')
   expect(stateWithEditingValue.editingValue).toBe('x + y')
   expect(stateWithEditingValue.watchExpressions).toEqual(state.watchExpressions)
 })
 
-test('should not change watchExpressions', () => {
-  const state = createDefaultState()
-  const stateWithExpression = addWatchExpression(state, 'a + b')
-  const stateWithEditingValue = handleWatchValueChange(stateWithExpression, 'x + y')
+test('should not change watchExpressions', async () => {
+  const state: RunAndDebugState = createDefaultState()
+  const stateWithExpression: RunAndDebugState = await addWatchExpression(state, 'a + b')
+  const stateWithEditingValue: RunAndDebugState = handleWatchValueChange(stateWithExpression, 'x + y')
   expect(stateWithEditingValue.editingValue).toBe('x + y')
   expect(stateWithEditingValue.watchExpressions).toEqual(stateWithExpression.watchExpressions)
 })
