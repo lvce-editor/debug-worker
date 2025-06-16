142d3845b10c8a772ac0ae678e815a2579fc5f56
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-16 23:40:04 +0200

    feature: improve value diff (#393)

--- packages/debug-worker/src/parts/DiffValue/DiffValue.ts
+++ packages/debug-worker/src/parts/DiffValue/DiffValue.ts
@@ -2,5 +2,8 @@ import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const isEqual = (oldState: RunAndDebugState, newState: RunAndDebugState): boolean => {
-  return newState.inputSource !== InputSource.Script && oldState.editingValue === newState.editingValue
+  if (newState.inputSource === InputSource.Script) {
+    return newState.editingValue === oldState.editingValue
+  }
+  return true
 }
