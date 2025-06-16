ff60ee031206d2156a1bbd9e4f5ce129003eea81
Le Vivilet <levivlet@pm.me>
2025-06-16 21:14:36 +0200

    tes

--- packages/debug-worker/test/EvaluateWatchExpression.test.ts
+++ packages/debug-worker/test/EvaluateWatchExpression.test.ts
@@ -1,12 +1,10 @@
-import { test, expect } from '@jest/globals'
+import { expect, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { evaluateWatchExpression } from '../src/parts/EvaluateWatchExpression/EvaluateWatchExpression.ts'
-import { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
-import { MockRpc } from '@lvce-editor/rpc'
-import * as RpcRegistry from '@lvce-editor/rpc-registry'
-import { RpcId } from '@lvce-editor/rpc-registry'
-import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
+import { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
 
 test('evaluateWatchExpression', async () => {
   const mockRpc = MockRpc.create({
