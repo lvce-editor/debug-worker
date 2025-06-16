e9be6ea38e5125265055929b5262f4a81d78e8fd
Le Vivilet <levivlet@pm.me>
2025-06-16 21:11:49 +0200

    test

--- packages/debug-worker/test/EvaluateWatchExpression.test.ts
+++ packages/debug-worker/test/EvaluateWatchExpression.test.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@jest/globals'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { evaluateWatchExpression } from '../src/parts/EvaluateWatchExpression/EvaluateWatchExpression.ts'
+import { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
 
 test('evaluateWatchExpression', async () => {
-  const state = createDefaultState()
+  const state: RunAndDebugState = createDefaultState()
   const expression = 'x + y'
-  const stateWithExpression = {
+  const stateWithExpression: RunAndDebugState = {
     ...state,
     watchExpressions: [
       {
@@ -14,6 +15,7 @@ test('evaluateWatchExpression', async () => {
         isEditing: false,
       },
     ],
+    debugId: 123,
   }
   const result = await evaluateWatchExpression(stateWithExpression, expression)
   expect(result.watchExpressions).toEqual([
