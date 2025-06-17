3614e1ec59068730cc40905698c3f3f6240ffac9
Le Vivilet <levivlet@pm.me>
2025-06-17 23:43:17 +0200

    feature: save state different only reload vs hot reload

--- packages/debug-worker/src/parts/SaveState/SaveState.ts
+++ packages/debug-worker/src/parts/SaveState/SaveState.ts
@@ -1,9 +1,10 @@
 import type { SavedState } from '../SavedState/SavedState.ts'
 import * as RunAndDebugStates from '../RunAndDebugStates/RunAndDebugStates.ts'
+import { SaveStateReasonHotReload } from '../SaveStateReason/SaveStateReason.ts'
 
-export const saveState = (uid: number): SavedState => {
+export const saveStateHotReload = (uid: number): SavedState => {
   const { newState } = RunAndDebugStates.get(uid)
-  const { watchExpressions, scopeExpanded, watchExpanded, breakPointsExpanded, focus, editingValue } = newState
+  const { watchExpressions, scopeExpanded, watchExpanded, breakPointsExpanded, focus, editingValue, inputSource } = newState
   return {
     watchExpressions,
     scopeExpanded,
@@ -11,5 +12,27 @@ export const saveState = (uid: number): SavedState => {
     watchExpanded,
     focus,
     editingValue,
+    inputSource,
   }
+}
+
+export const saveStateReload = (uid: number): SavedState => {
+  const hotReloadState = saveStateHotReload(uid)
+  return {
+    ...hotReloadState,
+    watchExpressions: hotReloadState.watchExpressions.map((expr) => ({
+      ...expr,
+      isEditing: false,
+    })),
+    focus: 0,
+    editingValue: '',
+    inputSource: 0,
+  }
+}
+
+export const saveState = (uid: number, reason: number): SavedState => {
+  if (reason === SaveStateReasonHotReload) {
+    return saveStateHotReload(uid)
+  }
+  return saveStateReload(uid)
 }
