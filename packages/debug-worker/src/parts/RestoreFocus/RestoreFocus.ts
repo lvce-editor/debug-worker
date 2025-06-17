3b4ca3d567a759d06344606a2fc8e3ed8cb0109f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-17 23:27:47 +0200

    fix: runtime error caused by restoreFocus (#421)

--- packages/debug-worker/src/parts/RestoreFocus/RestoreFocus.ts
+++ packages/debug-worker/src/parts/RestoreFocus/RestoreFocus.ts
@@ -1,8 +1,6 @@
-import { hasProperty } from '../HasProperty/HasProperty.ts'
-
 export const restoreFocus = (savedState: unknown): number => {
-  if (hasProperty(savedState, 'focus') && typeof savedState.focus === 'number') {
-    return savedState.focus
-  }
+  // if (hasProperty(savedState, 'focus') && typeof savedState.focus === 'number') {
+  //   return savedState.focus
+  // }
   return 0
 }
