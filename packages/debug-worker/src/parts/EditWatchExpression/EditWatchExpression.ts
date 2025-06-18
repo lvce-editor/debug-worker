08a6b5254f1142901518b706df996c72afb9eb3e
Le Vivilet <levivlet@pm.me>
2025-06-18 19:31:15 +0200

    feature: add editwatchexpression function

---
+++ packages/debug-worker/src/parts/EditWatchExpression/EditWatchExpression.ts
@@ -0,0 +1,24 @@
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import type { WatchExpression } from '../WatchExpression/WatchExpression.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+
+export const editWatchExpression = (state: RunAndDebugState, index: number): RunAndDebugState => {
+  const { watchExpressions } = state
+  const item = watchExpressions[index]
+  if (!item) {
+    return state
+  }
+  const newItem: WatchExpression = {
+    ...item,
+    isEditing: true,
+  }
+  const newWatchExpressions = watchExpressions.toSpliced(index, 1, newItem)
+  return {
+    ...state,
+    watchExpressions: newWatchExpressions,
+    focus: WhenExpression.FocusDebugWatchInput,
+    inputSource: InputSource.Script,
+    editingValue: item.expression,
+  }
+}
