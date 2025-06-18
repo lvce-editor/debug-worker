6d528c341d9286b5b9ee541e5bf147200d87c747
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-18 22:08:05 +0200

    feature: move actions dom into separate file (#463)

--- packages/debug-worker/src/parts/GetRunAndDebugRowSectionHeadingVirtualDom/GetRunAndDebugRowSectionHeadingVirtualDom.ts
+++ packages/debug-worker/src/parts/GetRunAndDebugRowSectionHeadingVirtualDom/GetRunAndDebugRowSectionHeadingVirtualDom.ts
@@ -1,32 +1,12 @@
-import type { DebugRow, DebugRowAction } from '../DebugRow/DebugRow.ts'
+import type { DebugRow } from '../DebugRow/DebugRow.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetActionsDom from '../GetActionsDom/GetActionsDom.ts'
 import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import * as VirtualDomHelpers from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-
-const getActionsDom = (actions: readonly DebugRowAction[]): readonly VirtualDomNode[] => {
-  const nodes: VirtualDomNode[] = []
-  nodes.push({
-    type: VirtualDomElements.Div,
-    className: ClassNames.DebugSectionActions,
-    childCount: actions.length,
-  })
-  for (const action of actions) {
-    nodes.push({
-      type: VirtualDomElements.Button,
-      className: ClassNames.DebugSectionAction,
-      title: action.title,
-      onClick: DomEventListenerFunctions.HandleClickSectionAction,
-      name: action.id,
-      childCount: 1,
-    })
-    nodes.push(VirtualDomHelpers.text(action.icon))
-  }
-  return nodes
-}
 
 export const renderSectionHeading = (row: DebugRow): readonly VirtualDomNode[] => {
   const { expanded, text, key, actions } = row
@@ -44,7 +24,7 @@ export const renderSectionHeading = (row: DebugRow): readonly VirtualDomNode[] =
     },
     expanded ? GetChevronVirtualDom.getChevronDownVirtualDom() : GetChevronVirtualDom.getChevronRightVirtualDom(),
     VirtualDomHelpers.text(text),
-    ...getActionsDom(actions || []),
+    ...GetActionsDom.getActionsDom(actions || []),
   ]
   return nodes
 }
