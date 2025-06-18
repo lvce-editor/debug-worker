11f93fa3e9e2b23be26a1811d37d2047f823b4f6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-18 23:52:14 +0200

    feature: update menu id (#480)

--- packages/debug-worker/src/parts/HandleWatchExpressionContextMenu/HandleWatchExpressionContextMenu.ts
+++ packages/debug-worker/src/parts/HandleWatchExpressionContextMenu/HandleWatchExpressionContextMenu.ts
@@ -1,9 +1,8 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
+import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleWatchExpressionContextMenu = async (state: RunAndDebugState, x: number, y: number, index: string): Promise<RunAndDebugState> => {
-  const menuId = 124 // TODO register context menu items for watch expressions
-  await ContextMenu.show(x, y, menuId)
-  // TODO: Implement watch expression context menu handling
+  await ContextMenu.show(x, y, MenuEntryId.DebugWatchExpression)
   return state
 }
