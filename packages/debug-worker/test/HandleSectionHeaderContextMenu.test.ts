98c06a0db76071ea42453db375ef5316770d7d67
Le Vivilet <levivlet@pm.me>
2025-06-18 22:36:50 +0200

    test

--- packages/debug-worker/test/HandleSectionHeaderContextMenu.test.ts
+++ packages/debug-worker/test/HandleSectionHeaderContextMenu.test.ts
@@ -2,11 +2,11 @@ import { test, expect } from '@jest/globals'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { handleSectionHeaderContextMenu } from '../src/parts/HandleSectionHeaderContextMenu/HandleSectionHeaderContextMenu.ts'
 
-test('handleSectionHeaderContextMenu returns the same state', () => {
+test('handleSectionHeaderContextMenu returns the same state', async () => {
   const state = createDefaultState()
   const x = 0
   const y = 0
   const id = ''
-  const result = handleSectionHeaderContextMenu(state, x, y, id)
+  const result = await handleSectionHeaderContextMenu(state, x, y, id)
   expect(result).toBe(state)
 })
