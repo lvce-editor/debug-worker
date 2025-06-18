d380c66289c4a8ab0d9eabdcacecab0e566d4d86
Le Vivilet <levivlet@pm.me>
2025-06-18 21:52:14 +0200

    feature: move paused2 into separate file

--- packages/debug-worker/src/parts/HandleChange/HandleChange.ts
+++ packages/debug-worker/src/parts/HandleChange/HandleChange.ts
@@ -1,40 +1,14 @@
 import type { ChangeParams } from '../ChangeParams/ChangeParams.ts'
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import * as DebugState from '../DebugState/DebugState.ts'
-import { evaluateWatchExpressions } from '../EvaluateWatchExpressions/EvaluateWatchExpressions.ts'
-import { getPausedInfo2 } from '../GetPausedInfo2/GetPausedInfo2.ts'
+import { handlePaused2 } from '../HandlePaused2/HandlePaused2.ts'
 import * as HandleResumed from '../HandleResumed/HandleResumed.ts'
-
-const handlePaused = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
-  const { debugId, watchExpressions } = state
-  try {
-    const { callFrameId, callStack, scopeChain, pausedMessage, pausedReason, expandedIds, scriptMap } = await getPausedInfo2(debugId)
-    // TODO move this to getPausedInfo2
-    const newWatchExpressions = await evaluateWatchExpressions(debugId, callFrameId, watchExpressions)
-    return {
-      ...state,
-      debugState: DebugState.Paused,
-      scopeChain,
-      scopeExpanded: true,
-      callStack,
-      pausedReason,
-      pausedMessage,
-      callFrameId,
-      expandedIds,
-      parsedScripts: scriptMap,
-      watchExpressions: newWatchExpressions,
-    }
-  } catch {
-    return state
-  }
-}
 
 export const handleChange = async (state: RunAndDebugState, params: ChangeParams): Promise<RunAndDebugState> => {
   if (params.type === 'resumed') {
     return HandleResumed.handleResumed(state)
   }
   if (params.type === 'paused') {
-    return handlePaused(state)
+    return handlePaused2(state)
   }
   return state
 }
