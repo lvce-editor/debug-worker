5f4b5c7f42ad32850fa461cdc0e707653a9eca1b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-18 19:32:17 +0200

    feature: add editwatchexpression function (#446)

--- packages/debug-worker/src/parts/HandleClickWatchExpression/HandleClickWatchExpression.ts
+++ packages/debug-worker/src/parts/HandleClickWatchExpression/HandleClickWatchExpression.ts
@@ -1,29 +1,11 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import type { WatchExpression } from '../WatchExpression/WatchExpression.ts'
-import * as InputSource from '../InputSource/InputSource.ts'
+import { editWatchExpression } from '../EditWatchExpression/EditWatchExpression.ts'
 import { parseIndex } from '../ParseIndex/ParseIndex.ts'
-import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const handleClickWatchExpression = async (state: RunAndDebugState, dataIndex: string, defaultPrevented: boolean): Promise<RunAndDebugState> => {
   if (defaultPrevented) {
     return state
   }
   const index = parseIndex(dataIndex)
-  const { watchExpressions } = state
-  const item = watchExpressions[index]
-  if (!item) {
-    return state
-  }
-  const newItem: WatchExpression = {
-    ...item,
-    isEditing: true,
-  }
-  const newWatchExpressions = watchExpressions.toSpliced(index, 1, newItem)
-  return {
-    ...state,
-    watchExpressions: newWatchExpressions,
-    focus: WhenExpression.FocusDebugWatchInput,
-    inputSource: InputSource.Script,
-    editingValue: item.expression,
-  }
+  return editWatchExpression(state, index)
 }
