2275b3a6dd15b472668e37c3a3f4b3ab901258f9
Le Vivilet <levivlet@pm.me>
2025-06-21 19:50:40 +0200

    feature: handle debug unavailable status

--- packages/debug-worker/src/parts/LoadContentLater/LoadContentLater.ts
+++ packages/debug-worker/src/parts/LoadContentLater/LoadContentLater.ts
@@ -1,8 +1,17 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as Debug from '../Debug/Debug.ts'
+import * as DebugState from '../DebugState/DebugState.ts'
+import * as ExtensionHostDebug from '../ExtensionHostDebug/ExtensionHostDebug.ts'
 
 export const loadContentLater = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { debugId } = state
   await Debug.start(debugId)
+  const status = await ExtensionHostDebug.getPausedStatus(debugId)
+  if (status && status.status === 'unavailable') {
+    return {
+      ...state,
+      debugState: DebugState.Unavailable,
+    }
+  }
   return state
 }
