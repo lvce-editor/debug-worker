5ede53ab353c307c4f9881a6e3787fcf51516414
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-22 00:23:23 +0200

    feature: simplify arrow code (#533)

--- packages/debug-worker/src/parts/HandleArrowLeft/HandleArrowLeft.ts
+++ packages/debug-worker/src/parts/HandleArrowLeft/HandleArrowLeft.ts
@@ -3,13 +3,13 @@ import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
 import { getRunAndDebugVisibleRows } from '../GetRunAndDebugVisibleRows/GetRunAndDebugVisibleRows.ts'
 import { getSectionClickHandler } from '../GetSectionClickHandler/GetSectionClickHandler.ts'
 
-export const handleArrowLeft = (state: RunAndDebugState): RunAndDebugState => {
+export const handleArrowLeft = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { selectedIndex } = state
   const rows = getRunAndDebugVisibleRows(state)
   const row = rows[selectedIndex]
   if (row && row.type === DebugRowType.SectionHeading && row.expanded) {
     const clickHandler = getSectionClickHandler(row.key)
-    return clickHandler(state) as RunAndDebugState
+    return clickHandler(state)
   }
   return state
 }
--- packages/debug-worker/test/HandleArrowRight.test.ts
+++ packages/debug-worker/test/HandleArrowRight.test.ts
@@ -5,13 +5,13 @@ import * as DebugRowType from '../src/parts/DebugRowType/DebugRowType.ts'
 import * as DebugSectionId from '../src/parts/DebugSectionId/DebugSectionId.ts'
 import { handleArrowRight } from '../src/parts/HandleArrowRight/HandleArrowRight.ts'
 
-test('handleArrowRight returns the same state', () => {
+test('handleArrowRight returns the same state', async () => {
   const state = createDefaultState()
-  const result = handleArrowRight(state)
+  const result = await handleArrowRight(state)
   expect(result).toBe(state)
 })
 
-test('handleArrowRight expands collapsed watch section heading', () => {
+test('handleArrowRight expands collapsed watch section heading', async () => {
   const state = {
     ...createDefaultState(),
     watchExpanded: false,
@@ -31,11 +31,11 @@ test('handleArrowRight expands collapsed watch section heading', () => {
       },
     ],
   }
-  const result = handleArrowRight(state)
+  const result = await handleArrowRight(state)
   expect(result.watchExpanded).toBe(true)
 })
 
-test('handleArrowRight does nothing if watch section heading is already expanded', () => {
+test('handleArrowRight does nothing if watch section heading is already expanded', async () => {
   const state = {
     ...createDefaultState(),
     watchExpanded: true,
@@ -55,6 +55,6 @@ test('handleArrowRight does nothing if watch section heading is already expanded
       },
     ],
   }
-  const result = handleArrowRight(state)
+  const result = await handleArrowRight(state)
   expect(result).toBe(state)
 })
