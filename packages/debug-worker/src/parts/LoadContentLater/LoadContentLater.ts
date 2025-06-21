bcc0b8e86bdb2168ad270406e71ddf4dc00c352c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-21 22:24:10 +0200

    feature: update visible rows in loadContentLater (#519)

--- packages/debug-worker/src/parts/LoadContentLater/LoadContentLater.ts
+++ packages/debug-worker/src/parts/LoadContentLater/LoadContentLater.ts
@@ -2,16 +2,18 @@ import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as Debug from '../Debug/Debug.ts'
 import * as DebugState from '../DebugState/DebugState.ts'
 import * as ExtensionHostDebug from '../ExtensionHostDebug/ExtensionHostDebug.ts'
+import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const loadContentLater = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { debugId } = state
   await Debug.start(debugId)
   const status = await ExtensionHostDebug.getPausedStatus(debugId)
   if (status && status.status === 'unavailable') {
-    return {
+    const newState: RunAndDebugState = {
       ...state,
       debugState: DebugState.Unavailable,
     }
+    return updateVisibleRows(newState)
   }
   return state
 }
