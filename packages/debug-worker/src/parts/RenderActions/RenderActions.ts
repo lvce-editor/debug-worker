d83074a52fa03362b766edc0cb802b13e48a1384
Le Vivilet <levivlet@pm.me>
2025-06-21 19:55:53 +0200

    feature: render empty actions when debug is unavailable

--- packages/debug-worker/src/parts/RenderActions/RenderActions.ts
+++ packages/debug-worker/src/parts/RenderActions/RenderActions.ts
@@ -1,10 +1,16 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import * as DebugState from '../DebugState/DebugState.ts'
 import * as GetRunAndDebugButtonsVirtualDom from '../GetRunAndDebugButtonsVirtualDom/GetRunAndDebugButtonsVirtualDom.ts'
 import * as RunAndDebugStates from '../RunAndDebugStates/RunAndDebugStates.ts'
 
 export const renderActions = (uid: number): readonly VirtualDomNode[] => {
   const { newState } = RunAndDebugStates.get(uid)
-  const { scopeExpanded } = newState
+  const { scopeExpanded, debugState } = newState
+
+  if (debugState === DebugState.Unavailable) {
+    return []
+  }
+
   if (scopeExpanded) {
     return GetRunAndDebugButtonsVirtualDom.getRunAndDebugButtonsVirtualDom(newState.debugState)
   }
