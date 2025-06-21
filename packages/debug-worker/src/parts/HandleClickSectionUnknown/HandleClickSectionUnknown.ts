76760124898e728510d629a287c6a56ecd89b921
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-21 23:00:12 +0200

    feature: don't change state when clicking on unknown section (#523)
    * feature: don't change state when clicking on unknown section
    
    * test

--- packages/debug-worker/src/parts/HandleClickSectionUnknown/HandleClickSectionUnknown.ts
+++ packages/debug-worker/src/parts/HandleClickSectionUnknown/HandleClickSectionUnknown.ts
@@ -1,11 +1,6 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const handleClickSectionUnkown = (state: RunAndDebugState): RunAndDebugState => {
-  const { callStackExpanded } = state
-  const newState = {
-    ...state,
-    callStackExpanded: !callStackExpanded,
-  }
-  return updateVisibleRows(newState)
+  // For unknown section IDs, return the state unchanged
+  return state
 }
