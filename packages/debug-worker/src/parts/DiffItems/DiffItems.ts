cd4071c70f35f6fe4a9cfe74f8d4095bd0e8e7d4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-22 00:27:30 +0200

    feature: rerender when selected index changes (#534)

--- packages/debug-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/debug-worker/src/parts/DiffItems/DiffItems.ts
@@ -5,14 +5,15 @@ export const diffType = DiffType.RenderItems
 
 export const isEqual = (oldState: RunAndDebugState, newState: RunAndDebugState): boolean => {
   return (
+    oldState.breakPointsExpanded === newState.breakPointsExpanded &&
+    oldState.callFrameId === newState.callFrameId &&
+    oldState.callStackExpanded === newState.callStackExpanded &&
+    oldState.debugState === newState.debugState &&
     oldState.scopeChain === newState.scopeChain &&
     oldState.scopeExpanded === newState.scopeExpanded &&
-    oldState.callFrameId === newState.callFrameId &&
-    oldState.breakPointsExpanded === newState.breakPointsExpanded &&
-    oldState.debugState === newState.debugState &&
+    oldState.selectedIndex === newState.selectedIndex &&
+    oldState.visibleRows === newState.visibleRows &&
     oldState.watchExpanded === newState.watchExpanded &&
-    oldState.callStackExpanded === newState.callStackExpanded &&
-    oldState.watchExpressions === newState.watchExpressions &&
-    oldState.visibleRows === newState.visibleRows
+    oldState.watchExpressions === newState.watchExpressions
   )
 }
