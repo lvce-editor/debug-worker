c419a3c7255ab817d3df88fc98b5c0e2fe7dca38
Le Vivilet <levivlet@pm.me>
2025-06-21 22:33:56 +0200

    feature: update visible rows on section click

--- packages/debug-worker/src/parts/HandleClickSectionScope/HandleClickSectionScope.ts
+++ packages/debug-worker/src/parts/HandleClickSectionScope/HandleClickSectionScope.ts
@@ -1,10 +1,12 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const handleClickSectionScope = (state: RunAndDebugState): RunAndDebugState => {
   const { scopeExpanded } = state
-  return {
+  const newState = {
     ...state,
     scopeExpanded: !scopeExpanded,
     focusedIndex: 2, // TODO don't hardcode number
   }
+  return updateVisibleRows(newState)
 }
