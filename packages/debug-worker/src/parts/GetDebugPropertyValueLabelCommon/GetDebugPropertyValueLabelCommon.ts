c8890441ced2ca6f05f88d4b16e032d4d95bfdf6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-21 15:49:40 +0200

    feature: add maxDescriptionLength property (#497)

--- packages/debug-worker/src/parts/GetDebugPropertyValueLabelCommon/GetDebugPropertyValueLabelCommon.ts
+++ packages/debug-worker/src/parts/GetDebugPropertyValueLabelCommon/GetDebugPropertyValueLabelCommon.ts
@@ -1,3 +1,7 @@
-export const getDebugPropertyValueLabelCommon = (property: any): any => {
-  return property.description
+export const getDebugPropertyValueLabelCommon = (property: any, maxDescriptionLength: number): string => {
+  const description = property.description
+  if (description && description.length > maxDescriptionLength) {
+    return description.slice(0, Math.max(0, maxDescriptionLength)) + '...'
+  }
+  return description
 }
--- packages/debug-worker/test/GetInnerChildScopeChain.test.ts
+++ packages/debug-worker/test/GetInnerChildScopeChain.test.ts
@@ -39,13 +39,13 @@ test('should return cached value if available', async () => {
   const cache = {
     'test-object-id': [{ type: 3, key: 'test', value: 'cached' }],
   }
-  const result = await getInnerChildScopeChain(cache, 'debug-id', 'test-object-id', 0)
+  const result = await getInnerChildScopeChain(cache, 'debug-id', 'test-object-id', 0, 100)
   expect(result).toEqual([{ type: 3, key: 'test', value: 'cached' }])
 })
 
 test('should get properties and create scope chain', async () => {
   const cache = {}
-  const result = await getInnerChildScopeChain(cache, 'debug-id', 'object-id', 0)
+  const result = await getInnerChildScopeChain(cache, 'debug-id', 'object-id', 0, 100)
   expect(result).toEqual([
     {
       type: 3,
