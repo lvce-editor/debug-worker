59a23833a3fdb19219e871b9438e754e139018a7
Le Vivilet <levivlet@pm.me>
2025-06-22 00:32:37 +0200

    simplify code

--- packages/debug-worker/src/parts/SelectPreviousRow/SelectPreviousRow.ts
+++ packages/debug-worker/src/parts/SelectPreviousRow/SelectPreviousRow.ts
@@ -1,19 +1,6 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import { clamp } from '../Clamp/Clamp.ts'
+import { selectIndex } from '../SelectIndex/SelectIndex.ts'
 
 export const selectPreviousRow = (state: RunAndDebugState): RunAndDebugState => {
-  const { selectedIndex, visibleRows } = state
-  if (visibleRows.length === 0) {
-    return {
-      ...state,
-      selectedIndex: -1,
-    }
-  }
-  const maxIndex = visibleRows.length - 1
-  const clampedIndex = clamp(selectedIndex, -1, maxIndex)
-  const newIndex = clamp(clampedIndex - 1, -1, maxIndex)
-  return {
-    ...state,
-    selectedIndex: newIndex,
-  }
+  return selectIndex(state, state.selectedIndex - 1)
 }
