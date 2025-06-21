e42bfc2841f46afec9af16b5a4d97e7c81731972
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-21 16:19:09 +0200

    feature: call refresh function when button is clicked (#505)
    * feature: call refresh function when button is clicked
    
    * lint

--- packages/debug-worker/src/parts/HandleClickSectionAction/HandleClickSectionAction.ts
+++ packages/debug-worker/src/parts/HandleClickSectionAction/HandleClickSectionAction.ts
@@ -1,6 +1,7 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { addWatchExpression } from '../AddWatchExpression/AddWatchExpression.ts'
 import * as InputName from '../InputName/InputName.ts'
+import { refreshWatchExpression } from '../RefreshWatchExpression/RefreshWatchExpression.ts'
 
 export const handleClickSectionAction = async (state: RunAndDebugState, actionId: string): Promise<RunAndDebugState> => {
   if (!actionId) {
@@ -9,6 +10,9 @@ export const handleClickSectionAction = async (state: RunAndDebugState, actionId
 
   if (actionId === InputName.AddWatchExpression) {
     return addWatchExpression(state, '')
+  }
+  if (actionId === InputName.RefreshWatchExpressions) {
+    return refreshWatchExpression(state)
   }
 
   return state
