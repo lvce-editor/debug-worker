031b800acffdfd4962b91f403d57551ec432190c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-21 22:21:54 +0200

    feature: update visible rows in handleChange (#518)

--- packages/debug-worker/src/parts/HandleChange/HandleChange.ts
+++ packages/debug-worker/src/parts/HandleChange/HandleChange.ts
@@ -2,13 +2,16 @@ import type { ChangeParams } from '../ChangeParams/ChangeParams.ts'
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { handlePaused2 } from '../HandlePaused2/HandlePaused2.ts'
 import * as HandleResumed from '../HandleResumed/HandleResumed.ts'
+import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const handleChange = async (state: RunAndDebugState, params: ChangeParams): Promise<RunAndDebugState> => {
+  let newState = state
+
   if (params.type === 'resumed') {
-    return HandleResumed.handleResumed(state)
+    newState = HandleResumed.handleResumed(state)
+  } else if (params.type === 'paused') {
+    newState = await handlePaused2(state)
   }
-  if (params.type === 'paused') {
-    return handlePaused2(state)
-  }
-  return state
+
+  return updateVisibleRows(newState)
 }
--- packages/debug-worker/src/parts/HandlePaused2/HandlePaused2.ts
+++ packages/debug-worker/src/parts/HandlePaused2/HandlePaused2.ts
@@ -2,6 +2,7 @@ import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import * as DebugState from '../DebugState/DebugState.ts'
 import { evaluateWatchExpressions } from '../EvaluateWatchExpressions/EvaluateWatchExpressions.ts'
 import { getPausedInfo2 } from '../GetPausedInfo2/GetPausedInfo2.ts'
+import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const handlePaused2 = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { debugId, watchExpressions } = state
@@ -9,7 +10,7 @@ export const handlePaused2 = async (state: RunAndDebugState): Promise<RunAndDebu
     const { callFrameId, callStack, scopeChain, pausedMessage, pausedReason, expandedIds, scriptMap } = await getPausedInfo2(debugId, state.maxDescriptionLength)
     // TODO move this to getPausedInfo2
     const newWatchExpressions = await evaluateWatchExpressions(debugId, callFrameId, watchExpressions)
-    return {
+    const newState = {
       ...state,
       debugState: DebugState.Paused,
       scopeChain,
@@ -22,6 +23,7 @@ export const handlePaused2 = async (state: RunAndDebugState): Promise<RunAndDebu
       parsedScripts: scriptMap,
       watchExpressions: newWatchExpressions,
     }
+    return updateVisibleRows(newState)
   } catch {
     return state
   }
