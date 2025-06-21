0bd089e19e52985150c4004e63b995b4ec82a879
Le Vivilet <levivlet@pm.me>
2025-06-21 21:52:58 +0200

    feature: add scopechain type

---
+++ packages/debug-worker/src/parts/ScopeChainItem/ScopeChainItem.ts
@@ -0,0 +1,9 @@
+export interface ScopeChainItem {
+  readonly indent: number
+  readonly key: string
+  readonly label: string
+  readonly objectId: string
+  readonly type: number
+  readonly value: string
+  readonly valueType: string
+}
