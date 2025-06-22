9790f35f88d9fc7999b82c19befd1aee23f69b45
Le Vivilet <levivlet@pm.me>
2025-06-22 10:16:09 +0200

    feature: add pause on exceptions diff

--- packages/debug-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/debug-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,10 +1,18 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffPauseOnExceptions from '../DiffPauseOnExceptions/DiffPauseOnExceptions.ts'
 import * as DiffSelection from '../DiffSelection/DiffSelection.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffItems.isEqual, DiffFocusContext.isEqual, DiffValue.isEqual, DiffFocus.isEqual, DiffSelection.isEqual]
+export const modules = [DiffItems.isEqual, DiffFocusContext.isEqual, DiffValue.isEqual, DiffFocus.isEqual, DiffSelection.isEqual, DiffPauseOnExceptions.isEqual]
 
-export const numbers = [DiffItems.diffType, DiffType.RenderFocusContext, DiffType.RenderValue, DiffType.RenderFocus, DiffType.RenderSelection]
+export const numbers = [
+  DiffItems.diffType,
+  DiffType.RenderFocusContext,
+  DiffType.RenderValue,
+  DiffType.RenderFocus,
+  DiffType.RenderSelection,
+  DiffPauseOnExceptions.diffType,
+]
