288d6b49fe23a9d77f3332ce2a1ee5ffae6f1bbb
Le Vivilet <levivlet@pm.me>
2025-06-22 10:28:58 +0200

    feature: update checkbox rendering

--- packages/debug-worker/src/parts/ApplyRender/ApplyRender.ts
+++ packages/debug-worker/src/parts/ApplyRender/ApplyRender.ts
@@ -1,9 +1,14 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
 import * as GetRenderer from '../GetRenderer/GetRenderer.ts'
 
 export const applyRender = (oldState: RunAndDebugState, newState: RunAndDebugState, diffResult: readonly number[]): readonly any[] => {
   const commands = []
   for (const item of diffResult) {
+    if (item === DiffType.RenderPauseOnExceptions) {
+      // TODO support this in the future
+      continue
+    }
     const fn = GetRenderer.getRenderer(item)
     commands.push(fn(oldState, newState))
   }
