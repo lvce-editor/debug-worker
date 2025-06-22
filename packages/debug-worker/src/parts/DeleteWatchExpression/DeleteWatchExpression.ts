4f1b1ef50625c2215f52a575b22c77b15536c4d2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-22 14:19:29 +0200

    feature: update rows when deleting watch expression (#550)

--- packages/debug-worker/src/parts/DeleteWatchExpression/DeleteWatchExpression.ts
+++ packages/debug-worker/src/parts/DeleteWatchExpression/DeleteWatchExpression.ts
@@ -1,11 +1,13 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const deleteWatchExpression = (state: RunAndDebugState, index: number): RunAndDebugState => {
   if (index < 0 || index >= state.watchExpressions.length) {
     return state
   }
-  return {
+  const newState: RunAndDebugState = {
     ...state,
     watchExpressions: state.watchExpressions.toSpliced(index, 1),
   }
+  return updateVisibleRows(newState)
 }
