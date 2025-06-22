c6bc4667970398e18a92341039a50acae03c6142
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-22 10:05:52 +0200

    feature: on space, toggle checkbox (#542)

--- packages/debug-worker/src/parts/HandleSpace/HandleSpace.ts
+++ packages/debug-worker/src/parts/HandleSpace/HandleSpace.ts
@@ -1,5 +1,20 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
+import { getCheckBoxClickHandler } from '../GetCheckBoxClickHandler/GetCheckBoxClickHandler.ts'
 
 export const handleSpace = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
+  const { selectedIndex, visibleRows } = state
+
+  if (selectedIndex < 0 || selectedIndex >= visibleRows.length) {
+    return state
+  }
+
+  const currentRow = visibleRows[selectedIndex]
+
+  if (currentRow.type === DebugRowType.CheckBox) {
+    const clickHandler = getCheckBoxClickHandler(currentRow.name)
+    return clickHandler(state)
+  }
+
   return state
 }
