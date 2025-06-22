1684ef925a15ac5723f2140cd309954d74e687ae
Le Vivilet <levivlet@pm.me>
2025-06-22 14:17:02 +0200

    feature: update rows when adding watch expression

--- packages/debug-worker/src/parts/AcceptWatchExpressionEdit/AcceptWatchExpressionEdit.ts
+++ packages/debug-worker/src/parts/AcceptWatchExpressionEdit/AcceptWatchExpressionEdit.ts
@@ -1,10 +1,11 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { getNewWatchExpressionsAccept } from '../GetNewWatchExpressionsAccept/GetNewWatchExpressionsAccept.ts'
+import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const acceptWatchExpressionEdit = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { watchExpressions, editingValue, debugId, debugState, callFrameId } = state
   const newWatchExpressions = await getNewWatchExpressionsAccept(debugId, callFrameId, debugState, watchExpressions, editingValue)
-  return {
+  const newState: RunAndDebugState = {
     ...state,
     watchExpressions: newWatchExpressions,
     focus: 0,
@@ -12,4 +13,5 @@ export const acceptWatchExpressionEdit = async (state: RunAndDebugState): Promis
     editingselectionstart: 0,
     editingselectionend: 0,
   }
+  return updateVisibleRows(newState)
 }
