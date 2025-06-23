c94d5af429fb77d3bb5bf8282a2590fe696074ab
Le Vivilet <levivlet@pm.me>
2025-06-23 23:39:46 +0200

    lint

--- packages/debug-worker/src/parts/HandleNew/HandleNew.ts
+++ packages/debug-worker/src/parts/HandleNew/HandleNew.ts
@@ -1,6 +1,6 @@
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { addWatchExpression } from '../AddWatchExpression/AddWatchExpression.ts'
 import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
-import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 
 export const handleNew = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { selectedIndex, visibleRows } = state
