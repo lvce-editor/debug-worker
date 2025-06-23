271fd098ed23138abec95afcc974200800046dd1
Le Vivilet <levivlet@pm.me>
2025-06-23 21:30:25 +0200

    feature: remove watch expression and reset editing value on cancel

--- packages/debug-worker/src/parts/GetNewWatchExpressionsCancel/GetNewWatchExpressionsCancel.ts
+++ packages/debug-worker/src/parts/GetNewWatchExpressionsCancel/GetNewWatchExpressionsCancel.ts
@@ -1,8 +1,11 @@
 import type { WatchExpression } from '../WatchExpression/WatchExpression.ts'
 
 export const getNewWatchExpressionsCancel = (watchExpressions: readonly WatchExpression[]): readonly WatchExpression[] => {
-  const lastExpression = watchExpressions[watchExpressions.length - 1]
+  const lastExpression = watchExpressions.at(-1)
   if (lastExpression && lastExpression.expression === '') {
+    return watchExpressions.toSpliced(-1, 1)
+  }
+  if (lastExpression && lastExpression.isEditing) {
     return watchExpressions.toSpliced(-1, 1)
   }
   return watchExpressions
