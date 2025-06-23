98f80766fa56f64133324c8b42233ee3af4b4257
Le Vivilet <levivlet@pm.me>
2025-06-23 21:17:10 +0200

    simplify

--- packages/debug-worker/src/parts/HandleNew/HandleNew.ts
+++ packages/debug-worker/src/parts/HandleNew/HandleNew.ts
@@ -1,13 +1,11 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { addWatchExpression } from '../AddWatchExpression/AddWatchExpression.ts'
 import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
-import { getRunAndDebugVisibleRows } from '../GetRunAndDebugVisibleRows/GetRunAndDebugVisibleRows.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const handleNew = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
-  const { selectedIndex } = state
-  const rows = getRunAndDebugVisibleRows(state)
-  const row = rows[selectedIndex]
+  const { selectedIndex, visibleRows } = state
+  const row = visibleRows[selectedIndex]
 
   if (row && (row.type === DebugRowType.WatchExpression || (row.type === DebugRowType.InputField && row.name === InputName.WatchExpressionInput))) {
     return addWatchExpression(state, '')
