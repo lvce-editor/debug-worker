7c407828caae35f3f14928221b96482213e52a85
Le Vivilet <levivlet@pm.me>
2025-06-23 11:07:55 +0200

    feature: add expressionresult type

--- packages/debug-worker/src/parts/EvaluateWatchExpression/EvaluateWatchExpression.ts
+++ packages/debug-worker/src/parts/EvaluateWatchExpression/EvaluateWatchExpression.ts
@@ -1,6 +1,7 @@
+import type { ExpressionResult } from '../ExpressionResult/ExpressionResult.ts'
 import * as Debug from '../Debug/Debug.ts'
 
-export const evaluateWatchExpression = async (debugId: any, callFrameId: any, expression: string): Promise<any> => {
+export const evaluateWatchExpression = async (debugId: any, callFrameId: any, expression: string): Promise<ExpressionResult> => {
   const result = await Debug.evaluate(debugId, expression, callFrameId)
   return result
 }
