01a1fdba1a6851738dac42129662b87cceee666e
Le Vivilet <levivlet@pm.me>
2025-06-23 23:35:36 +0200

    feature: update handleNew condition

--- packages/debug-worker/src/parts/HandleNew/HandleNew.ts
+++ packages/debug-worker/src/parts/HandleNew/HandleNew.ts
@@ -1,13 +1,11 @@
-import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 import { addWatchExpression } from '../AddWatchExpression/AddWatchExpression.ts'
 import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
-import * as InputName from '../InputName/InputName.ts'
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
 
 export const handleNew = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
   const { selectedIndex, visibleRows } = state
   const row = visibleRows[selectedIndex]
-
-  if (row && (row.type === DebugRowType.WatchExpression || (row.type === DebugRowType.InputField && row.name === InputName.WatchExpressionInput))) {
+  if (row && (row.type === DebugRowType.WatchExpression || row.type === DebugRowType.WatchMessage)) {
     return addWatchExpression(state, '')
   }
 
