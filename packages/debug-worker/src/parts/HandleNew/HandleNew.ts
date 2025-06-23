d88606db4535b577f8c2740bbb9c47f72a7d2390
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-23 21:18:55 +0200

    feature: add new watch expression with ctrl+n (#589)
    * feature: add new watch expression with ctrl+n
    
    * simplify
    
    * test

--- packages/debug-worker/src/parts/HandleNew/HandleNew.ts
+++ packages/debug-worker/src/parts/HandleNew/HandleNew.ts
@@ -1,6 +1,15 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { addWatchExpression } from '../AddWatchExpression/AddWatchExpression.ts'
+import * as DebugRowType from '../DebugRowType/DebugRowType.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const handleNew = async (state: RunAndDebugState): Promise<RunAndDebugState> => {
-  // TODO: Implement new functionality
+  const { selectedIndex, visibleRows } = state
+  const row = visibleRows[selectedIndex]
+
+  if (row && (row.type === DebugRowType.WatchExpression || (row.type === DebugRowType.InputField && row.name === InputName.WatchExpressionInput))) {
+    return addWatchExpression(state, '')
+  }
+
   return state
 }
