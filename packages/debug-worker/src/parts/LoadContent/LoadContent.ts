4cead90f3a4f9a3761f66f44b45e48e4c23303a0
Le Vivilet <levivlet@pm.me>
2025-06-23 10:57:34 +0200

    feature: don't restore state on test

--- packages/debug-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/debug-worker/src/parts/LoadContent/LoadContent.ts
@@ -5,6 +5,9 @@ import { restoreState } from '../RestoreState/RestoreState.ts'
 import { updateVisibleRows } from '../UpdateVisibleRows/UpdateVisibleRows.ts'
 
 export const loadContent = async (state: RunAndDebugState, isTest: boolean, savedState: unknown): Promise<RunAndDebugState> => {
+  if (isTest) {
+    savedState = {}
+  }
   const { watchExpressions, watchExpanded, breakPointsExpanded, scopeExpanded, focus, editingValue } = restoreState(savedState)
   const debugId = getDebugId(isTest)
   const newState = {
