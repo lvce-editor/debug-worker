742f49634aa7c586d7748f51b9ed9f74dc3a6e7a
Le Vivilet <levivlet@pm.me>
2025-06-24 13:43:46 +0200

    feature: use scopechain item type

--- packages/debug-worker/src/parts/GetVisibleScopeItems/GetVisibleScopeItems.ts
+++ packages/debug-worker/src/parts/GetVisibleScopeItems/GetVisibleScopeItems.ts
@@ -1,3 +1,4 @@
+import type { ScopeChainItem } from '../ScopeChainItem/ScopeChainItem.ts'
 import * as DebugItemFlags from '../DebugItemFlags/DebugItemFlags.ts'
 
 const getExpandable = (valueType: any): boolean => {
@@ -20,7 +21,7 @@ const getFlags = (isExpanded: boolean, isExpandable: boolean, isFocused: boolean
   return flags
 }
 
-export const getVisibleScopeItems = (scopeChain: any, expandedIds: any, focusedIndex: any): readonly any[] => {
+export const getVisibleScopeItems = (scopeChain: readonly ScopeChainItem[], expandedIds: any, focusedIndex: any): readonly ScopeChainItem[] => {
   const minLineY = 0
   const maxLineY = scopeChain.length
   const visible = []
