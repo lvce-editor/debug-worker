49b7c109339faf20e5e0f7fe352262def95da117
Le Vivilet <levivlet@pm.me>
2025-06-24 13:41:57 +0200

    simplify code

--- packages/debug-worker/src/parts/HandleClickScopeValue/HandleClickScopeValue.ts
+++ packages/debug-worker/src/parts/HandleClickScopeValue/HandleClickScopeValue.ts
@@ -1,9 +1,7 @@
 import { collapseScopeChain } from '../CollapseScopeChain/CollapseScopeChain.ts'
 import { expandScopeChain } from '../ExpandScopeChain/ExpandScopeChain.ts'
-import * as Focus from '../Focus/Focus.ts'
 import * as MouseEventType from '../MouseEventType/MouseEventType.ts'
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 // TODO pass index to function instead of text
 export const handleClickScopeValue = async (state: RunAndDebugState, dataIndex: string, button: number): Promise<RunAndDebugState> => {
@@ -13,7 +11,6 @@ export const handleClickScopeValue = async (state: RunAndDebugState, dataIndex: 
   }
 
   const { scopeChain, debugId, expandedIds, visibleRows } = state
-  Focus.setFocus(WhenExpression.FocusDebugScope)
 
   const listIndex = Number.parseInt(dataIndex, 10)
   if (isNaN(listIndex) || listIndex < 0 || listIndex >= visibleRows.length) {
