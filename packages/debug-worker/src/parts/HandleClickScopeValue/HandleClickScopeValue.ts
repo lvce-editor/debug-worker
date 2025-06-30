5b58775ff7e5cfe7cedda609a24e0a06c558849c
Le Vivilet <levivlet@pm.me>
2025-06-30 16:59:24 +0200

    feature: add handleClickIndex function

--- packages/debug-worker/src/parts/HandleClickScopeValue/HandleClickScopeValue.ts
+++ packages/debug-worker/src/parts/HandleClickScopeValue/HandleClickScopeValue.ts
@@ -1,32 +1,8 @@
 import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
-import { collapseScopeChain } from '../CollapseScopeChain/CollapseScopeChain.ts'
-import { expandScopeChain } from '../ExpandScopeChain/ExpandScopeChain.ts'
-import * as MouseEventType from '../MouseEventType/MouseEventType.ts'
+import { handleClickScopeValueIndex } from '../HandleClickScopeValueIndex/HandleClickScopeValueIndex.ts'
 import { parseIndex } from '../ParseIndex/ParseIndex.ts'
 
-// TODO pass index to function instead of text
 export const handleClickScopeValue = async (state: RunAndDebugState, dataIndex: string, button: number): Promise<RunAndDebugState> => {
-  // Return state unchanged if not left click
-  if (button !== MouseEventType.LeftClick) {
-    return state
-  }
-
-  const { scopeChain, debugId, expandedIds, visibleRows } = state
-
   const listIndex = parseIndex(dataIndex)
-  if (listIndex < 0 || listIndex >= visibleRows.length) {
-    return state
-  }
-
-  const row = visibleRows[listIndex]
-  if (!row.index || row.index < 0 || row.index >= scopeChain.length) {
-    return state
-  }
-
-  const scopeChainIndex = row.index
-  const element = scopeChain[scopeChainIndex]
-  if (expandedIds.includes(element.objectId)) {
-    return collapseScopeChain(state, expandedIds, scopeChain, element, scopeChainIndex)
-  }
-  return expandScopeChain(state, expandedIds, scopeChain, element, scopeChainIndex, debugId)
+  return handleClickScopeValueIndex(state, listIndex)
 }
---
+++ packages/debug-worker/src/parts/HandleClickScopeValueIndex/HandleClickScopeValueIndex.ts
@@ -0,0 +1,23 @@
+import type { RunAndDebugState } from '../RunAndDebugState/RunAndDebugState.ts'
+import { collapseScopeChain } from '../CollapseScopeChain/CollapseScopeChain.ts'
+import { expandScopeChain } from '../ExpandScopeChain/ExpandScopeChain.ts'
+
+export const handleClickScopeValueIndex = async (state: RunAndDebugState, index: number): Promise<RunAndDebugState> => {
+  const { scopeChain, debugId, expandedIds, visibleRows } = state
+
+  if (index < 0 || index >= visibleRows.length) {
+    return state
+  }
+
+  const row = visibleRows[index]
+  if (!row.index || row.index < 0 || row.index >= scopeChain.length) {
+    return state
+  }
+
+  const scopeChainIndex = row.index
+  const element = scopeChain[scopeChainIndex]
+  if (expandedIds.includes(element.objectId)) {
+    return collapseScopeChain(state, expandedIds, scopeChain, element, scopeChainIndex)
+  }
+  return expandScopeChain(state, expandedIds, scopeChain, element, scopeChainIndex, debugId)
+}
