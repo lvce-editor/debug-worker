bfa23f652cec30d325c8a72637677c40943b5f06
Le Vivilet <levivlet@pm.me>
2025-07-01 14:32:08 +0200

    fix: error when collapsing last scopechain item

--- packages/debug-worker/src/parts/ScopeChainItem/ScopeChainItem.ts
+++ packages/debug-worker/src/parts/ScopeChainItem/ScopeChainItem.ts
@@ -1,11 +1,11 @@
 export interface ScopeChainItem {
+  readonly flags: number
   readonly indent: number
+  readonly index?: number
   readonly key: string
   readonly label: string
   readonly objectId: string
   readonly type: number
   readonly value: string
   readonly valueType: string
-  readonly flags: number
-  readonly index?: number
 }
