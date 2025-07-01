16e507fccdb65a00bae577305e738e0c679b045d
Le Vivilet <levivlet@pm.me>
2025-07-01 10:35:23 +0200

    tsc

--- packages/debug-worker/test/GetHighlight.test.ts
+++ packages/debug-worker/test/GetHighlight.test.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@jest/globals'
-import type { CallStackItem } from '../src/parts/CallStackItem/CallStackItem.ts'
 import type { RunAndDebugState } from '../src/parts/RunAndDebugState/RunAndDebugState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { getHighlight } from '../src/parts/GetHighlight/GetHighlight.ts'
