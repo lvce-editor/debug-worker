9141eb1d48f4e2445ebc4a3e7bb7abe1cf55c259
Le Vivilet <levivlet@pm.me>
2025-07-05 21:44:21 +0200

    feature: simplify code and reduce memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 490_000
 
-export const instantiations = 15_000
+export const instantiations = 6_000
 
 export const instantiationsPath = join(root, 'packages', 'debug-worker')
 
