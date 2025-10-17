fcdcf32e49ecd9537ed4d101c93c94912a33fac0
Le Vivilet <levivilet@pm.me>
2025-10-17 09:26:29 +0200

    skip

--- packages/e2e/src/sample.debug-provider-watch-expression-edit.ts
+++ packages/e2e/src/sample.debug-provider-watch-expression-edit.ts
@@ -2,6 +2,8 @@ import { type Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.debug-provider-edit-watch-expression'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, FileSystem, Workspace, Extension, SideBar, Locator, expect }) => {
   // arrange
   await SideBar.open('Explorer')
