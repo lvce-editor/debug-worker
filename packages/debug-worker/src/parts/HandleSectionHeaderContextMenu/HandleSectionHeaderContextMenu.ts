0408eb92e061fe511d697b91f081e1e6dc893a12
Le Vivilet <levivilet@pm.me>
2025-11-26 13:06:21 +0100

    undo

--- packages/debug-worker/src/parts/HandleSectionHeaderContextMenu/HandleSectionHeaderContextMenu.ts
+++ packages/debug-worker/src/parts/HandleSectionHeaderContextMenu/HandleSectionHeaderContextMenu.ts
@@ -3,9 +3,6 @@ import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleSectionHeaderContextMenu = async (state: RunAndDebugState, x: number, y: number, id: string): Promise<RunAndDebugState> => {
-  const { uid: id } = state
-  await ContextMenu.show2(id, MenuEntryId.DebugWatchSectionHeading, x, y, {
-    menuId: MenuEntryId.DebugWatchSectionHeading,
-  })
+  await ContextMenu.show(x, y, MenuEntryId.DebugWatchSectionHeading)
   return state
 }
