56a1b5bce3b2ecc3378d73504899ef8c768ac75b
Le Vivilet <levivilet@pm.me>
2026-07-26 09:19:38 +0200

    feature: migrate from lerna to npm workspaces (#730)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,22 +16,21 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x rollup -x lerna -x @babel/preset-typescript -x typescript`
-  SUB='All dependencies match the latest package versions'
-  if [[ "$OUTPUT" == *"$SUB"* ]]; then
-    echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
-  fi
+  ncu -u -x @types/node -x rollup -x @babel/preset-typescript -x typescript
 }
 
-                                                       updateDependencies             &&
-cd packages/build                                   && updateDependencies && cd ../.. &&
-cd packages/server                                  && updateDependencies && cd ../.. &&
-cd packages/debug-worker                            && updateDependencies && cd ../.. &&
-cd packages/e2e                                     && updateDependencies && cd ../.. &&
-cd packages/memory                                  && updateDependencies && cd ../.. &&
+updateDependencies
+
+for package_dir in packages/*; do
+  if [[ -f "$package_dir/package.json" ]]; then
+    (
+      cd "$package_dir"
+      updateDependencies
+    )
+  fi
+done
+
+nice npm install
 
 echo "Great Success!"
 
